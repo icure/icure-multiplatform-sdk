9bcc39c6646253a51dc6335f970245491fc35d1f
Vincenzo Pierro <vcp@icure.com>
2024-03-15 16:16:19 +0100

    Added ZonedDateTime

--- icure-sdk/src/commonMain/kotlin/com/icure/sdk/api/raw/BaseRawApi.kt
+++ icure-sdk/src/commonMain/kotlin/com/icure/sdk/api/raw/BaseRawApi.kt
@@ -1,14 +1,33 @@
 package com.icure.sdk.api.raw
 
 import io.ktor.client.*
+import io.ktor.client.plugins.*
 import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
+import kotlin.time.Duration
 
-abstract class  BaseRawApi {
+abstract class BaseRawApi(
+	additionalHeaders: Map<String, String>,
+	requestTimeout : Duration?
+) {
 
 	protected val httpClient = HttpClient {
 		install(ContentNegotiation) {
 			json()
+		}
+		requestTimeout?.also {
+			install(HttpTimeout) {
+				requestTimeoutMillis = requestTimeout.inWholeMilliseconds
+			}
+		}
+
+		headers {
+			additionalHeaders.filterKeys {
+				it.lowercase() != "content-type"
+			}.forEach { (header, headerValue) ->
+				set(header, headerValue)
+			}
 		}
 	}
 }
