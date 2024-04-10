30e3317c84a46d72d3acdab1f7f3266ec1b2139c
Luca Tremamunno <luca@icure.com>
2024-04-10 18:08:56 +0200

    Implement graph methods

---
+++ icure-sdk/src/commonMain/kotlin/com/icure/sdk/crypto/impl/TransferKeysManagerImpl.kt
@@ -0,0 +1,12 @@
+package com.icure.sdk.crypto.impl
+
+import com.icure.sdk.crypto.TransferKeysManager
+import com.icure.sdk.model.CryptoActorStubWithType
+import com.icure.sdk.utils.InternalIcureApi
+
+@InternalIcureApi
+class TransferKeysManagerImpl : TransferKeysManager {
+	override suspend fun updateTransferKeys(self: CryptoActorStubWithType) {
+		TODO("Not yet implemented")
+	}
+}
