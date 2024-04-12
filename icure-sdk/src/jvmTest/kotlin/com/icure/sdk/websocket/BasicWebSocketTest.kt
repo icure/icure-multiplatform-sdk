3b07a95e99d71c0e5456f0d62270ca9baa65cc89
Clément VANDENDAELEN <cv@icure.com>
2024-04-12 14:25:52 +0200

    Splitted tests

--- icure-sdk/src/jvmTest/kotlin/com/icure/sdk/websocket/BasicWebSocketTest.kt
+++ icure-sdk/src/jvmTest/kotlin/com/icure/sdk/websocket/BasicWebSocketTest.kt
@@ -1,14 +1,11 @@
 package com.icure.sdk.websocket
 
 import com.icure.sdk.model.DecryptedHealthElement
-import com.icure.sdk.model.DecryptedPatient
 import com.icure.sdk.model.filter.healthelement.HealthElementByHcPartyFilter
 import com.icure.sdk.model.notification.SubscriptionEventType
-import com.icure.sdk.test.DataOwnerDetails
-import com.icure.sdk.test.createHcpUser
-import com.icure.sdk.test.initialiseTestEnvironment
 import io.kotest.assertions.fail
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
 import io.ktor.server.application.install
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
@@ -22,6 +19,7 @@ import io.mockk.coEvery
 import io.mockk.mockk
 import kotlinx.coroutines.CompletableDeferred
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withTimeoutOrNull
 import kotlinx.serialization.json.Json
@@ -32,91 +30,46 @@ import kotlin.time.Duration.Companion.seconds
 @OptIn(InternalAPI::class)
 class BasicWebSocketTest : StringSpec({
 
-	lateinit var  hcpUser: DataOwnerDetails
-
-	beforeSpec {
-		initialiseTestEnvironment()
-		hcpUser = createHcpUser()
-	}
-
-	"Should subscribe to HealthElement updates" {
-		val completable = CompletableDeferred<Unit>()
-
-		val connection = hcpUser
-			.api()
-			.healthElement
-			.encrypted
-			.subscribeToEvents(
-				events = setOf(SubscriptionEventType.Create),
-				filter = HealthElementByHcPartyFilter(
-					hcpId = hcpUser.dataOwnerId,
-				),
-				onConnected = {
-					println("Connected")
-					completable.complete(Unit)
+	val fakeWebsocketServer = embeddedServer(Netty, port = 25565) {
+		install(WebSockets)
+		routing {
+			webSocket {
+				launch {
+					for (frame in incoming) {
+						when (frame) {
+							is Frame.Text -> {
+								val content = frame.readText()
+								println("SERVER: Received message: $content")
+							}
+							else -> println("SERVER: Received frame: $frame")
+						}
+					}
 				}
-			) {
-				println(it.id)
+				println("SERVER: Sending ping")
+				send(Frame.Text("ping"))
+				send(Frame.Text(Json.encodeToString(DecryptedHealthElement.serializer(), DecryptedHealthElement(id = UUID.randomUUID().toString()))))
 			}
-
-		withTimeoutOrNull(5.seconds) {
-			completable.await()
-			connection.close()
-
-		} ?: fail("Didn't received OPEN event within 5 seconds")
-	}
-
-	"Should be able to listen to HealthElement creation updates" {
-		val completable = CompletableDeferred<Unit>()
-
-		val connection = hcpUser
-			.api()
-			.healthElement
-			.encrypted
-			.subscribeToEvents(
-				setOf(SubscriptionEventType.Create),
-				HealthElementByHcPartyFilter(
-					hcpId = hcpUser.dataOwnerId,
-				),
-			) {
-				println("Received HealthElement creation event for ${it.id}")
-				completable.complete(Unit)
-			}.also { connection ->
-				connection.onReconnected {
-					println("Connected")
+			webSocket("/load") {
+				launch {
+					for (frame in incoming) {
+						when (frame) {
+							is Frame.Text -> {
+								val content = frame.readText()
+								println("SERVER: Received message: $content")
+							}
+							else -> println("SERVER: Received frame: $frame")
+						}
+					}
+				}
+				repeat(100) {
+					send(Frame.Text(Json.encodeToString(DecryptedHealthElement.serializer(), DecryptedHealthElement(id = UUID.randomUUID().toString()))))
 				}
 			}
+		}
+	}.start(wait = false)
 
-		val patient = hcpUser
-			.api()
-			.patient
-			.initialiseEncryptionMetadata(
-				DecryptedPatient(id = UUID.randomUUID().toString())
-			).let {
-				hcpUser.api().patient.encryptAndCreate(it)
-			}
-
-		val currentUser = hcpUser.api().user.getCurrentUser()
-
-		hcpUser
-			.api()
-			.healthElement
-			.initialiseEncryptionMetadata(
-				DecryptedHealthElement(id = UUID.randomUUID().toString()),
-				patient!!,
-				currentUser
-			).let {
-				hcpUser
-					.api()
-					.healthElement
-					.createHealthcareElement(it)
-				println("Created HealthElement")
-			}
-
-		withTimeoutOrNull(5.seconds) {
-			completable.await()
-			connection.close()
-		} ?: fail("Didn't received MESSAGE event within 5 seconds")
+	afterSpec {
+		fakeWebsocketServer.stop(0, 0)
 	}
 
 	"Should be able to reconnect if didn't received a ping within the configured delay" {
@@ -126,29 +79,9 @@ class BasicWebSocketTest : StringSpec({
 			coEvery { getBearerToken() } returns "token"
 		}
 
-		val ktorServer = embeddedServer(Netty, port = 25565) {
-				install(WebSockets)
-				routing {
-					webSocket {
-						launch {
-							for (frame in incoming) {
-								when (frame) {
-									is Frame.Text -> {
-										val content = frame.readText()
-										println("SERVER: Received message: $content")
-									}
-									else -> println("SERVER: Received frame: $frame")
-								}
-							}
-						}
-						println("SERVER: Sending ping")
-						send(Frame.Text("ping"))
-						send(Frame.Text(Json.encodeToString(DecryptedHealthElement.serializer(), DecryptedHealthElement(id = UUID.randomUUID().toString()))))
-					}
-				}
-		}.start(wait = false)
-
-		delay(5.seconds)
+		while (!fakeWebsocketServer.application.isActive) {
+			delay(1.seconds)
+		}
 
 		val connection = ConnectionImpl.initialize(
 			hostname = "localhost:25565",
@@ -167,7 +100,7 @@ class BasicWebSocketTest : StringSpec({
 			retryDelay = 500.milliseconds,
 			retryDelayExponentFactor = 2.0,
 			maxRetries = 5,
-			durationBetweenPings = 2.seconds,
+			durationBetweenPings = 1.seconds,
 			eventCallback = { element ->
 				println("CLIENT: Received HealthElement event for ${element.id}")
 			},
@@ -177,11 +110,64 @@ class BasicWebSocketTest : StringSpec({
 			println("CLIENT: Reconnected")
 			connection.close()
 			completable.complete(Unit)
-			ktorServer.stop(0, 0)
 		}
 
 		withTimeoutOrNull(10.seconds) {
 			completable.await()
-		} ?: fail("Didn't reconnected within 50 seconds")
+		} ?: fail("Didn't reconnected within 10 seconds")
+	}
+
+	"Should close the connection if the queue is full" {
+		val completableDisconnected = CompletableDeferred<Unit>()
+		val completableFatal = CompletableDeferred<Boolean>()
+
+		val authProvider = mockk<WebSocketAuthProvider>() {
+			coEvery { getBearerToken() } returns "token"
+		}
+
+		while (!fakeWebsocketServer.application.isActive) {
+			delay(1.seconds)
+		}
+
+		val connection = ConnectionImpl.initialize(
+			hostname = "localhost:25565",
+			path = "/load",
+			events = setOf(SubscriptionEventType.Create),
+			filter = HealthElementByHcPartyFilter(
+				hcpId = "fake-uuid",
+			),
+			qualifiedName = "com.icure.sdk.model.health.HealthElement",
+			subscriptionSerializer = { "" },
+			serializer = DecryptedHealthElement.serializer(),
+			webSocketAuthProvider = authProvider,
+			onOpenListener = {
+				println("CLIENT: Connected")
+			},
+			retryDelay = 1.seconds,
+			retryDelayExponentFactor = 2.0,
+			maxRetries = 5,
+			durationBetweenPings = 5.seconds,
+			eventCallback = { element ->
+				println("CLIENT: Received HealthElement event for ${element.id}")
+				delay(1.seconds) // Simulate processing time
+			},
+		)
+
+		connection.onError { errorMessage, fatal ->
+			println("CLIENT: Error $errorMessage")
+			if (fatal) {
+				completableFatal.complete(fatal)
+			}
+		}
+
+		connection.onDisconnected { code, reason ->
+			println("CLIENT: Disconnected with code $code and reason $reason")
+			completableDisconnected.complete(Unit)
+		}
+
+		withTimeoutOrNull(20.seconds) {
+			completableFatal.await() shouldBe true
+			completableDisconnected.await()
+		} ?: fail("Didn't reconnected within 10 seconds")
 	}
 })
---
+++ icure-sdk/src/jvmTest/kotlin/com/icure/sdk/websocket/SubscriptionsTests.kt
@@ -0,0 +1,122 @@
+package com.icure.sdk.websocket
+
+import com.icure.sdk.model.DecryptedHealthElement
+import com.icure.sdk.model.DecryptedPatient
+import com.icure.sdk.model.base.Identifiable
+import com.icure.sdk.model.filter.AbstractFilter
+import com.icure.sdk.model.filter.healthelement.HealthElementByHcPartyFilter
+import com.icure.sdk.model.notification.SubscriptionEventType
+import com.icure.sdk.test.DataOwnerDetails
+import com.icure.sdk.test.createHcpUser
+import com.icure.sdk.test.initialiseTestEnvironment
+import io.kotest.assertions.fail
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.core.spec.style.stringSpec
+import io.kotest.matchers.should
+import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withTimeoutOrNull
+import java.util.UUID
+import kotlin.time.Duration.Companion.seconds
+
+fun <BaseType : Identifiable<String>, MaybeDecryptedType : BaseType> subscribableTests(
+	name: String,
+	subscribableApi: Subscribable<BaseType, MaybeDecryptedType>,
+	createEntity: suspend () -> Any,
+	filter: AbstractFilter<BaseType>,
+) = stringSpec {
+	name should {
+		"Should subscribe to $name updates" {
+			val completable = CompletableDeferred<Unit>()
+
+			val connection = subscribableApi
+				.subscribeToEvents(
+					events = setOf(SubscriptionEventType.Create),
+					filter = filter,
+					onConnected = {
+						println("Connected")
+						completable.complete(Unit)
+					},
+				) {
+					println(it.id)
+				}
+
+			withTimeoutOrNull(5.seconds) {
+				completable.await()
+				connection.close()
+
+			} ?: fail("Didn't received OPEN event within 5 seconds")
+		}
+
+		"Should be able to listen to $name creation updates" {
+			val completable = CompletableDeferred<Unit>()
+
+			val connection = subscribableApi
+				.subscribeToEvents(
+					setOf(SubscriptionEventType.Create),
+					filter,
+				) {
+					println("Received HealthElement creation event for ${it.id}")
+					completable.complete(Unit)
+				}.also { connection ->
+					connection.onReconnected {
+						println("Connected")
+					}
+				}
+
+			createEntity()
+
+			withTimeoutOrNull(5.seconds) {
+				completable.await()
+				connection.close()
+			} ?: fail("Didn't received MESSAGE event within 5 seconds")
+		}
+	}
+}
+
+class SubscriptionsTests : StringSpec(
+	{
+
+		lateinit var hcpUser: DataOwnerDetails
+
+		runBlocking {
+			initialiseTestEnvironment()
+			hcpUser = createHcpUser()
+
+			include(
+				subscribableTests(
+					name = "HealthElement",
+					subscribableApi = hcpUser.api().healthElement.encrypted,
+					filter = HealthElementByHcPartyFilter(hcpId = hcpUser.dataOwnerId),
+					createEntity =  {
+						val patient = hcpUser
+							.api()
+							.patient
+							.initialiseEncryptionMetadata(
+								DecryptedPatient(id = UUID.randomUUID().toString())
+							).let {
+								hcpUser.api().patient.encryptAndCreate(it)
+							}
+
+						val currentUser = hcpUser.api().user.getCurrentUser()
+
+						hcpUser
+							.api()
+							.healthElement
+							.initialiseEncryptionMetadata(
+								DecryptedHealthElement(id = UUID.randomUUID().toString()),
+								patient!!,
+								currentUser
+							).let {
+								hcpUser
+									.api()
+									.healthElement
+									.createHealthcareElement(it)
+								println("Created HealthElement")
+							}
+					}
+				),
+			)
+		}
+	},
+)
