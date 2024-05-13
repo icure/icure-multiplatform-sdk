fab8eb3fd0bd18059510042c8d0582c3643b6734
Luca Tremamunno <tremaluca96@gmail.com>
2024-05-13 16:33:54 +0200

    Fix type of entity template

--- icure-sdk/src/commonMain/kotlin/com/icure/sdk/model/EntityTemplate.kt
+++ icure-sdk/src/commonMain/kotlin/com/icure/sdk/model/EntityTemplate.kt
@@ -3,13 +3,7 @@ package com.icure.sdk.model
 import com.icure.sdk.model.base.StoredDocument
 import com.icure.sdk.serialization.EntityTemplateSerializer
 import kotlinx.serialization.Serializable
-import kotlin.Any
-import kotlin.Boolean
-import kotlin.Long
-import kotlin.String
-import kotlin.collections.List
-import kotlin.collections.Map
-import kotlin.collections.Set
+import kotlinx.serialization.json.JsonElement
 
 // WARNING: This file is auto-generated. If you change it manually, your changes will be lost.
 // If you want to change the way this class is generated, see [this repo](https://github.com/icure/sdk-codegen).
@@ -25,7 +19,7 @@ data class EntityTemplate(
 	public val entityType: String? = null,
 	public val subType: String? = null,
 	public val defaultTemplate: Boolean? = null,
-	public val entity: List<Map<String, Any>> = emptyList(),
+	public val entity: List<JsonElement> = emptyList(),
 ) : StoredDocument {
 	// region EntityTemplate-EntityTemplate
 
