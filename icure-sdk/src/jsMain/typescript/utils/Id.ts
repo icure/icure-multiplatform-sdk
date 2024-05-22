5df44a0b4eacf8a4c23a35e52d99d8a94fa9f786
Luca Tremamunno <tremaluca96@gmail.com>
2024-05-22 13:53:25 +0200

    Add random uuid generation utils function

--- icure-sdk/src/jsMain/typescript/utils/Id.ts
+++ icure-sdk/src/jsMain/typescript/utils/Id.ts
@@ -1,1 +1,14 @@
-export function randomUuid() { throw "TODO" }
+let crypto: Crypto
+
+export function randomUuid(): string {
+  if (!crypto) {
+    if (typeof window !== 'undefined') {
+      crypto = window.crypto
+    } else if (typeof self !== 'undefined') {
+      crypto = self.crypto
+    } else throw new Error(
+      "Can't generate uuid as crypto is not available. Initialising an instance of iCure API with a crypto service for this environment should solve the issue."
+    )
+  }
+  return crypto.randomUUID()
+}
