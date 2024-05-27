28816e3682ea6ab0d12bbf47ee0cadfa0d9715c9
Vincenzo Pierro <vcp@icure.com>
2024-05-27 11:18:13 +0200

    Fixed filter typing in X api

--- icure-sdk/src/commonMain/kotlin/com/icure/sdk/api/flavoured/ContactApi.kt
+++ icure-sdk/src/commonMain/kotlin/com/icure/sdk/api/flavoured/ContactApi.kt
@@ -55,8 +55,8 @@ import kotlinx.serialization.json.jsonObject
 
 /* This interface includes the API calls that do not need encryption keys and do not return or consume encrypted/decrypted items, they are completely agnostic towards the presence of encrypted items */
 interface ContactBasicFlavourlessApi {
-	suspend fun matchContactsBy(filter: AbstractFilter<EncryptedContact>): List<String>
-	suspend fun matchServicesBy(filter: AbstractFilter<EncryptedService>): List<String>
+	suspend fun matchContactsBy(filter: AbstractFilter<Contact>): List<String>
+	suspend fun matchServicesBy(filter: AbstractFilter<Service>): List<String>
 	suspend fun deleteContact(entityId: String): DocIdentifier
 	suspend fun deleteContacts(entityIds: List<String>): List<DocIdentifier>
 	suspend fun findContactsDelegationsStubsByHcPartyPatientForeignKeys(
@@ -73,7 +73,7 @@ interface ContactBasicFlavouredApi<E : Contact, S : Service> {
 	suspend fun modifyContacts(entities: List<E>): List<E>
 	suspend fun getContact(entityId: String): E
 	suspend fun getContacts(entityIds: List<String>): List<E>
-	suspend fun filterContactsBy(filterChain: FilterChain<EncryptedContact>, startDocumentId: String?, limit: Int?): PaginatedList<E>
+	suspend fun filterContactsBy(filterChain: FilterChain<Contact>, startDocumentId: String?, limit: Int?): PaginatedList<E>
 
 	suspend fun listContactByHCPartyServiceId(hcPartyId: String, serviceId: String): List<E>
 	suspend fun listContactsByExternalId(externalId: String): List<E>
@@ -101,7 +101,7 @@ interface ContactBasicFlavouredApi<E : Contact, S : Service> {
 		limit: Int? = null,
 	): PaginatedList<E>
 
-	suspend fun filterServicesBy(filterChain: FilterChain<EncryptedService>, startDocumentId: String?, limit: Int?): PaginatedList<S>
+	suspend fun filterServicesBy(filterChain: FilterChain<Service>, startDocumentId: String?, limit: Int?): PaginatedList<S>
 }
 
 /* The extra API calls declared in this interface are the ones that can be used on encrypted or decrypted items but only when the user is a data owner */
@@ -156,7 +156,7 @@ private abstract class AbstractContactBasicFlavouredApi<E : Contact, S : Service
 		rawApi.getContacts(ListOfIds(entityIds)).successBody().map { maybeDecrypt(it) }
 
 	override suspend fun filterContactsBy(
-		filterChain: FilterChain<EncryptedContact>,
+		filterChain: FilterChain<Contact>,
 		startDocumentId: String?,
 		limit: Int?,
 	): PaginatedList<E> =
@@ -188,7 +188,7 @@ private abstract class AbstractContactBasicFlavouredApi<E : Contact, S : Service
 
 	override suspend fun getService(serviceId: String): S = rawApi.getService(serviceId).successBody().let { maybeDecryptService(it) }
 	override suspend fun filterServicesBy(
-		filterChain: FilterChain<EncryptedService>,
+		filterChain: FilterChain<Service>,
 		startDocumentId: String?,
 		limit: Int?,
 	): PaginatedList<S> =
@@ -342,8 +342,8 @@ internal fun Service.asIcureStubWithTypeInfo(): EntityWithTypeInfo<IcureStub> {
 
 @InternalIcureApi
 private class AbstractContactBasicFlavourlessApi(val rawApi: RawContactApi) : ContactBasicFlavourlessApi {
-	override suspend fun matchContactsBy(filter: AbstractFilter<EncryptedContact>) = rawApi.matchContactsBy(filter).successBody()
-	override suspend fun matchServicesBy(filter: AbstractFilter<EncryptedService>): List<String> = rawApi.matchServicesBy(filter).successBody()
+	override suspend fun matchContactsBy(filter: AbstractFilter<Contact>) = rawApi.matchContactsBy(filter).successBody()
+	override suspend fun matchServicesBy(filter: AbstractFilter<Service>): List<String> = rawApi.matchServicesBy(filter).successBody()
 	override suspend fun deleteContact(entityId: String) = rawApi.deleteContact(entityId).successBody()
 	override suspend fun deleteContacts(entityIds: List<String>) = rawApi.deleteContacts(ListOfIds(entityIds)).successBody()
 	override suspend fun findContactsDelegationsStubsByHcPartyPatientForeignKeys(
