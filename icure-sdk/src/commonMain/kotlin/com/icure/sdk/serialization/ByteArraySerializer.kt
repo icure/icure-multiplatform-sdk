3b60593f71aff7e333e13c5c2d04dc73d5ac7195
Clément VANDENDAELEN <cv@icure.com>
2024-05-29 15:39:04 +0200

    Fixed ByteArray serial descriptor name

--- icure-sdk/src/commonMain/kotlin/com/icure/sdk/serialization/ByteArraySerializer.kt
+++ icure-sdk/src/commonMain/kotlin/com/icure/sdk/serialization/ByteArraySerializer.kt
@@ -10,7 +10,7 @@ import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 
 object ByteArraySerializer: KSerializer<ByteArray> {
-	override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("ByteArray", PrimitiveKind.STRING)
+	override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("ByteArrayBase64", PrimitiveKind.STRING)
 	override fun deserialize(decoder: Decoder): ByteArray = base64Decode(decoder.decodeString())
 	override fun serialize(encoder: Encoder, value: ByteArray) = encoder.encodeString(base64Encode(value))
 }
