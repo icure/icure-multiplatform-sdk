6774729e186732044686d85498fccf54f98a37b7
Clément VANDENDAELEN <cv@icure.com>
2024-05-29 09:32:58 +0200

    Fixed AbstractFilter serialization statements

---
+++ icure-sdk/src/jvmTest/kotlin/com/icure/sdk/SetBodyTest.kt
@@ -0,0 +1,95 @@
+package com.icure.sdk
+
+import com.icure.sdk.model.HealthcareParty
+import com.icure.sdk.model.base.Identifiable
+import com.icure.sdk.model.base.Identifier
+import com.icure.sdk.model.filter.AbstractFilter
+import com.icure.sdk.model.filter.UnionFilter
+import com.icure.sdk.model.filter.chain.FilterChain
+import com.icure.sdk.model.filter.hcparty.HealthcarePartyByIdentifiersFilter
+import com.icure.sdk.model.filter.hcparty.HealthcarePartyByIdsFilter
+import com.icure.sdk.utils.Serialization
+import com.icure.sdk.utils.newPlatformHttpClient
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.*
+import io.ktor.client.statement.bodyAsText
+import io.ktor.http.*
+import io.ktor.http.content.TextContent
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.Contextual
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.JsonArray
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.JsonPrimitive
+import kotlinx.serialization.json.encodeToJsonElement
+
+class SetBodyTest: StringSpec({
+
+	"Should be capable of setting body" {
+		val client = newPlatformHttpClient {
+			install(ContentNegotiation) {
+				json(json = Serialization.json)
+			}
+		}
+
+		val filter = HealthcarePartyByIdentifiersFilter(
+			identifiers = listOf(Identifier("UUID",))
+		)
+
+		val serialized = Serialization.json.encodeToString<@Contextual AbstractFilter<HealthcareParty>>(filter)
+
+		val response = client.post {
+			url {
+				takeFrom(Url("https://echo.free.beeceptor.com"))
+				appendPathSegments(
+					"sample-request"
+				)
+			}
+			contentType(ContentType.Application.Json)
+			setBody(TextContent(Serialization.json.encodeToString<@Contextual AbstractFilter<HealthcareParty>>(filter), ContentType.Application.Json))
+		}
+
+		val textFulBody = response.bodyAsText()
+		println(textFulBody)
+
+
+	}
+
+	"Should be able to serialize filter" {
+		val filterSpecific: AbstractFilter<HealthcareParty> = HealthcarePartyByIdsFilter(setOf("whatever"))
+		val filterGeneric: AbstractFilter<out Identifiable<String>> = filterSpecific
+		val filterJson = JsonObject(mapOf(
+			"\$type" to JsonPrimitive("HealthcarePartyByIdsFilter"),
+			"ids" to JsonArray(listOf(JsonPrimitive("whatever"))),
+		))
+		val unionFilterSpecific: UnionFilter<HealthcareParty> = UnionFilter(filters = listOf(filterSpecific))
+		val unionFilterGeneric: UnionFilter<out Identifiable<String>> = unionFilterSpecific
+		val unionFilterJson = JsonObject(mapOf(
+			"\$type" to JsonPrimitive("UnionFilter"),
+			"filters" to JsonArray(listOf(filterJson)),
+		))
+		val unionFilterNoTypeJson = JsonObject(mapOf(
+			"filters" to JsonArray(listOf(filterJson)),
+		))
+		val filterChainUnionSpecific = FilterChain(unionFilterSpecific)
+		val filterChainUnionGeneric = FilterChain(unionFilterGeneric)
+		val filterChainUnionJson = JsonObject(mapOf(
+			"filter" to unionFilterJson,
+		))
+		val filterChainByIdsJson = JsonObject(mapOf(
+			"filter" to filterJson,
+		))
+		Serialization.json.encodeToJsonElement(FilterChain(unionFilterSpecific)) shouldBe filterChainUnionJson
+		Serialization.json.encodeToJsonElement(FilterChain(unionFilterGeneric)) shouldBe filterChainUnionJson
+		Serialization.json.encodeToJsonElement(FilterChain(filterSpecific)) shouldBe filterChainByIdsJson
+		Serialization.json.encodeToJsonElement(FilterChain(filterGeneric)) shouldBe filterChainByIdsJson
+		Serialization.json.encodeToJsonElement(unionFilterSpecific) shouldBe unionFilterNoTypeJson
+		Serialization.json.encodeToJsonElement(unionFilterGeneric) shouldBe unionFilterNoTypeJson
+		// Currently there is a bug with `encodeToJsonElement` https://github.com/Kotlin/kotlinx.serialization/issues/2535
+		Serialization.json.encodeToString<@Contextual AbstractFilter<HealthcareParty>>(filterSpecific) shouldBe filterJson.toString()
+		Serialization.json.encodeToString<@Contextual AbstractFilter<out Identifiable<String>>>(filterGeneric) shouldBe filterJson.toString()
+	}
+
+})
