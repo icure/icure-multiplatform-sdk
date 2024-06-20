266efeea727589cdf8de330bbeacdfbf6e38cc62
Luca Tremamunno <tremaluca96@gmail.com>
2024-06-20 17:46:49 +0200

    Change py result conversion strategy

---
+++ python-wrapper/src/linuxMain/kotlin/com/icure/sdk/py/utils/PyResult.kt
@@ -0,0 +1,134 @@
+package com.icure.sdk.py.utils
+
+import com.icure.sdk.utils.Serialization
+import kotlinx.cinterop.ByteVarOf
+import kotlinx.cinterop.CFunction
+import kotlinx.cinterop.COpaquePointer
+import kotlinx.cinterop.CPointer
+import kotlinx.cinterop.CValues
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.StableRef
+import kotlinx.cinterop.cstr
+import kotlinx.cinterop.invoke
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.JsonPrimitive
+import kotlinx.serialization.json.encodeToJsonElement
+
+
+private fun Throwable.stringForPy() =
+	stackTraceToString()
+
+/**
+ * Converts the kotlin result to a json representation of result understandable by python.
+ * Used by method that only returns data entities without any logic / entities that can be safely serialized.
+ *
+ * The caller of the method will be in charge of disposing the string.
+ */
+internal inline fun <reified T> Result<T>.toPyString() =
+	fold(
+		onSuccess = { res ->
+			JsonObject(mapOf("success" to Serialization.json.encodeToJsonElement(res)))
+		},
+		onFailure = { e ->
+			JsonObject(mapOf("failure" to JsonPrimitive(e.stringForPy())))
+		}
+	).toString()
+
+/**
+ * Calls the python callback with the result as a string.
+ * Used by method that only returns data entities without any logic / entities that can be safely serialized.
+ *
+ * The string should not be used after the callback terminates, since the strings will be automatically disposed by
+ * kotlin after the callback completes (they should be copied or decoded before the callback returns).
+ */
+@OptIn(ExperimentalForeignApi::class)
+internal inline fun <reified T> Result<T>.toPyStringAsyncCallback(
+	callback: CPointer<CFunction<(result: CValues<ByteVarOf<Byte>>?, error: CValues<ByteVarOf<Byte>>?) -> Unit>>
+) =
+	fold(
+		onSuccess = { res ->
+			callback.invoke(Serialization.json.encodeToString(res).cstr, null)
+		},
+		onFailure = { e ->
+			callback.invoke(null, e.stringForPy().cstr)
+		}
+	).toString()
+
+/**
+ * If the result is a failure calls the callback providing the error.
+ *
+ * The error string should not be used after the callback terminates, since it will be automatically disposed by
+ * kotlin after the callback completes (it should be copied or decoded before the callback returns).
+ */
+@OptIn(ExperimentalForeignApi::class)
+internal inline fun <reified T> Result<T>.failureToPyStringAsyncCallback(
+	callback: CPointer<CFunction<(result: CValues<ByteVarOf<Byte>>?, error: CValues<ByteVarOf<Byte>>?) -> Unit>>
+): Unit {
+	onFailure { e ->
+		callback.invoke(null, e.stringForPy().cstr)
+	}
+}
+
+
+@OptIn(ExperimentalForeignApi::class)
+class PyResult internal constructor(
+	val success: COpaquePointer?,
+	val failure: String?
+)
+
+/**
+ * Converts the kotlin result to a result in an appropriate format for python. If this result is a success it contains a
+ * direct reference to the kotlin object instance, and therefore is suitable for cases where the output has some logic.
+ *
+ * The caller will be in charge of disposing the PyResult by:
+ * - Using the DisposeStablePointer utility method on the result itself
+ * - Using the DisposeString utility method on the failure component if not null
+ * - Using the DisposeStablePointer on the success if not null (will be not null only if failure is not null, but may
+ *   be null if failure is null). This value may still be use after it is not needed anymore.
+ */
+@OptIn(ExperimentalForeignApi::class)
+internal fun Result<*>.toPyResult() =
+	fold(
+		onSuccess = { res ->
+			PyResult(res?.let { StableRef.create(it).asCPointer() }, null)
+		},
+		onFailure = { e ->
+			PyResult(null, e.stringForPy())
+		}
+	)
+
+/**
+ * Converts the kotlin result to a result in an appropriate format for python. This result contains a direct reference
+ * to the kotlin object instance, and therefore is suitable for cases where the output has some logic.
+ *
+ * The error string will be disposed automatically by kotlin after the callback completes, but the successful result (if
+ * not null) needs to be disposed manually by the caller.
+ */
+@OptIn(ExperimentalForeignApi::class)
+internal fun Result<*>.toPyResultAsyncCallback(
+	callback: CPointer<CFunction<(result: COpaquePointer?, error: CValues<ByteVarOf<Byte>>?) -> Unit>>
+) =
+	fold(
+		onSuccess = { res ->
+			callback.invoke(res?.let { StableRef.create(it).asCPointer() }, null)
+		},
+		onFailure = { e ->
+			callback.invoke(null, e.stringForPy().cstr)
+		}
+	)
+
+/**
+ * If the result is a failure calls the callback providing the error.
+ *
+ * The error string should not be used after the callback terminates, since it will be automatically disposed by
+ * kotlin after the callback completes (it should be copied or decoded before the callback returns).
+ */
+@OptIn(ExperimentalForeignApi::class)
+internal inline fun <reified T> Result<T>.failureToPyStringAsyncCallback(
+	callback: CPointer<CFunction<(result: COpaquePointer?, error: CValues<ByteVarOf<Byte>>?) -> Unit>>
+): Unit {
+	onFailure { e ->
+		callback.invoke(null, e.stringForPy().cstr)
+	}
+}
