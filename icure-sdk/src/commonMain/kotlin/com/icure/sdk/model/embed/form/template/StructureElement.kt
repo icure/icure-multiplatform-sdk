970bcd61ec8fbad33eaa35d35de3896896bac98d
Clément Vandendaelen <vandendaelenclement@gmail.com>
2024-06-21 15:36:32 +0200

    Updated models and APIs (#110)
    * Updated models and APIs 202406211244-9abe2379b2790ce20ef04ad1808634b2d0dd7bf9
    
    * fix: Fixed auto-generated code
    
    * Add access to internal crypto api for tests
    
    * fix: Fixed listDocumentsByHcPartyMessageForeignKeys DocumentAPI call
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Luca Tremamunno <tremaluca96@gmail.com>

--- icure-sdk/src/commonMain/kotlin/com/icure/sdk/model/embed/form/template/StructureElement.kt
+++ icure-sdk/src/commonMain/kotlin/com/icure/sdk/model/embed/form/template/StructureElement.kt
@@ -5,7 +5,6 @@ import kotlinx.serialization.Serializable
 // WARNING: This file is auto-generated. If you change it manually, your changes will be lost.
 // If you want to change the way this class is generated, see [this repo](https://github.com/icure/sdk-codegen).
 
-//@Serializable
 sealed interface StructureElement {
 	// region StructureElement-StructureElement
 
