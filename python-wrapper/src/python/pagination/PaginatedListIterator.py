188ad3cca5bc1f323cadc907fc9536d0aceaa933
Luca Tremamunno <tremaluca96@gmail.com>
2024-06-27 09:45:28 +0200

    Implement blocking of paginated list iterator

--- python-wrapper/src/python/pagination/PaginatedListIterator.py
+++ python-wrapper/src/python/pagination/PaginatedListIterator.py
@@ -1,5 +1,7 @@
 from typing import Generic, TypeVar, List, Optional, Callable
-from ctypes import c_void_p
+from ctypes import c_void_p, cast, c_char_p
+from kotlin_types import symbols
+from model.CallResult import create_result_from_json
 
 T = TypeVar('T')
 
@@ -9,15 +11,27 @@ class PaginatedListIterator(Generic[T]):
         self.producer = producer
         self.deserializer = deserializer
 
-    def has_next_blocking(self, size: Optional[int] = None) -> bool:
-        TODO()
+    def has_next_blocking(self) -> bool:
+        call_result = symbols.kotlin.root.icure.sdk.py.utils.PaginatedListIterator.hasNextBlocking(self.producer)
+        result_info = create_result_from_json(cast(call_result, c_char_p).value.decode('utf-8'))
+        symbols.DisposeString(call_result)
+        if "failure" in result_info and result_info.get("failure") is not None:
+            raise Exception(result_info["failure"])
+        else:
+            return result_info["success"]
 
-    def has_next_async(self, size: Optional[int] = None) -> bool:
-        TODO()
+    def has_next_async(self) -> bool:
+        raise "TODO"
 
-    def next_async(self, size: int = 1) -> List[T]:
-        TODO()
+    def next_blocking(self, limit: int) -> List[T]:
+        call_result = symbols.kotlin.root.icure.sdk.py.utils.PaginatedListIterator.nextBlocking(self.producer, limit)
+        result_info = create_result_from_json(cast(call_result, c_char_p).value.decode('utf-8'))
+        symbols.DisposeString(call_result)
+        if "failure" in result_info and result_info.get("failure") is not None:
+            raise Exception(result_info["failure"])
+        else:
+            return result_info["success"]  # TODO deserialize
 
-    def next_blocking(self, size: int = 1) -> List[T]:
-        TODO()
+    def next_async(self, limit: int) -> List[T]:
+        raise "TODO"
 
