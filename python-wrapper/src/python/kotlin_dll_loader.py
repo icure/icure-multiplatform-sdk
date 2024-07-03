637a8cf8fd0aa8f40acc542cf60847c799dac86b
Vincenzo Pierro <vcp@icure.com>
2024-07-03 17:12:17 +0200

    Updated dll loader and Document Api

--- python-wrapper/src/python/kotlin_dll_loader.py
+++ python-wrapper/src/python/kotlin_dll_loader.py
@@ -1,3 +1,14 @@
+import platform
 from ctypes import CDLL
 
-kdll = CDLL('../../build/bin/macosArm64/releaseShared/libicure_sdk_native_pylib.dylib')
+arch = platform.machine()
+system = platform.system().lower()
+
+if system == 'linux' and arch == 'x86_64':
+    kdll = CDLL('../../build/bin/macosArm64/releaseShared/libicure_sdk_native_pylib.dylib')
+elif system == 'darwin' and arch == 'x86_64':
+    kdll = CDLL('../../build/bin/macosArm64/releaseShared/libicure_sdk_native_pylib.dylib')
+elif system == 'darwin' and arch == 'arm64':
+    kdll = CDLL('../../build/bin/macosArm64/releaseShared/libicure_sdk_native_pylib.dylib')
+else:
+    raise Exception(f"Your system ({system} {arch}) is not compatible with this version of the iCure SDK")
