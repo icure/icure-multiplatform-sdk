44febe1c6036ab158d10965b99e5c449a5a6dafb
Luca Tremamunno <tremaluca96@gmail.com>
2024-07-12 16:45:53 +0200

    Add windows to loader

--- python-wrapper/src/python/kotlin_dll_loader.py
+++ python-wrapper/src/python/kotlin_dll_loader.py
@@ -5,16 +5,23 @@ import os
 
 arch = platform.machine()
 system = platform.system().lower()
+extension = None
 dependencies = []
 package = None
 
 if system == 'linux' and arch == 'x86_64':
     dependencies = ["libcurl.so.4.8.0"]
     package = 'icure.lib.linuxX64'
+    extension = 'so'
 elif system == 'darwin' and arch == 'x86_64':
     package = 'icure.lib.macosX64'
+    extension = 'dylib'
 elif system == 'darwin' and arch == 'arm64':
     package = 'icure.lib.macosArm64'
+    extension = 'dylib'
+elif system == 'windows' and arch == 'AMD64':
+    package = 'icure.lib.mingwX64'
+    extension = 'dll'
 else:
     raise Exception(f"Your system ({system} {arch}) is not compatible with this version of the iCure SDK")
 
@@ -22,5 +29,5 @@ for d in dependencies:
     with path(package, d) as d_path:
         CDLL(str(d_path))
 
-with path(package, 'icure-sdk.so') as lib_path:
+with path(package, f"icure-sdk.{extension}") as lib_path:
     kdll = CDLL(str(lib_path))
