23da847f6cd2cba8429c7f52404cdcf473d46123
Luca Tremamunno <tremaluca96@gmail.com>
2024-07-18 17:28:31 +0200

    Add Patient and Contact filter builders

---
+++ python-wrapper/src/python/filters/ContactFilters.py
@@ -0,0 +1,202 @@
+from icure.model import ContactAbstractFilter, ContactByHcPartyFilter, ContactByHcPartyIdentifiersFilter, \
+    ContactByServiceIdsFilter, ContactByHcPartyTagCodeDateFilter, ContactByHcPartyPatientTagCodeDateFilter, \
+    ComplementFilter, Identifier, Patient
+from icure import IcureSdk
+from icure.filters.AbstractFilterBuilder import AbstractFilterBuilder
+from typing import List, Optional, Union
+
+
+class ContactFilters:
+    """
+    Factory and builder for creating contact filters.
+    """
+
+    @classmethod
+    def complement_filter(cls, super_set: ContactAbstractFilter, sub_set: ContactAbstractFilter) -> ContactAbstractFilter:
+        """
+        :param super_set: a filter
+        :param sub_set: a filter
+        :return: A filter that provides all entities that are in super_set but not in subset. The result will preserve
+        the same ordering it had in the super_set.
+        """
+        return ComplementFilter(super_set=super_set, sub_set=sub_set)
+
+    @classmethod
+    def all_contacts_for_self_filter(
+            cls,
+            sdk: IcureSdk
+    ) -> ContactAbstractFilter:
+        """
+        :param sdk: the sdk, used to get the id of the current data owner
+        :return: A filter to get all contacts accessible to the current data owner
+        """
+        return ContactByHcPartyFilter(sdk.data_owner.get_current_data_owner_id_blocking())
+
+    @classmethod
+    def all_contacts_for_data_owner_filter(
+            cls,
+            data_owner_id: str
+    ) -> ContactAbstractFilter:
+        """
+        :param data_owner_id: a data owner id. Note that you may need some special permissions to use the filter if the
+        data owner id is not the current data owner.
+        :return: A filter to get all contacts accessible to the specified data owner
+        """
+        return ContactByHcPartyFilter(data_owner_id)
+
+    class Builder(AbstractFilterBuilder[ContactAbstractFilter, 'ContactFilters.Builder']):
+        """
+        Allows to build complex filters for contacts
+        """
+
+        def __init__(self, sdk: IcureSdk, data_owner_id: Optional[str] = None):
+            """
+            Initializes the filter builder
+            :param sdk: the sdk, used to get some contextual information when needed.
+            :param data_owner_id: id of a data owner to use in the filter. For most filters only data that is accessible to
+            that data owner will be returned. If None will use the id of the data owner logged into the sdk.
+            """
+            self.__sdk = sdk
+            self.__data_owner_id = data_owner_id if data_owner_id is not None else sdk.data_owner.get_current_data_owner_id_blocking()
+
+        def by_identifiers(self, identifiers: List[Identifier], sort: bool = False) -> 'ContactFilters.Builder':
+            """
+            The filter will return all the entities that have at least one of the specified identifiers. The system and
+            value of an identifier must match exactly for an entity to be returned by this filter (other properties
+            of the identifier are ignored).
+            :param identifiers: a list of identifiers
+            :param sort: if True the data obtained through this filter will be returned in the same order as the input
+            identifiers. In case an entity has multiple identifiers only the first matching identifier will be
+            considered.
+            :raises ValueError: if sort is True and another filter was previously set as the sort key.
+            :return: self
+            """
+            return self._add_filter(
+                ContactByHcPartyIdentifiersFilter(
+                    identifiers=identifiers,
+                    healthcare_party_id=self.__data_owner_id
+                ),
+                sort
+            )
+
+        def by_code(
+            self,
+            code_type: str,
+            code_code: Optional[str],
+            start_of_contact_opening_date: Optional[int],
+            end_of_contact_opening_date: Optional[int],
+            sort: bool = False
+        ) -> 'ContactFilters.Builder':
+            """
+            Get all contacts that have a certain code. If you specify only the code type you will get all entities that
+            have at least a code of that type. You can also limit the result to only contacts that are within a
+            certain opening date timeframe, but in that case you must specify the code_code.
+            :param code_type: a code type
+            :param code_code: a code for the provided code type, or None if you want the filter to accept any entity
+            with a code of the provided type.
+            :param start_of_contact_opening_date: if provided the filters accepts only contacts with an opening date
+            that is after this value (inclusive).
+            :param end_of_contact_opening_date: if provided the filters accepts only contacts with an opening date
+            that is before this value (inclusive).
+            :param sort: if True the data obtained through this filter will be sorted by code type, code code, and then
+            opening date
+            raises ValueError: if sort is True and another filter was previously set as the sort key, or if you provide
+            a range for the opening date but no code code.
+            :return: self
+            """
+            if (start_of_contact_opening_date is not None or end_of_contact_opening_date is not None) and code_code is None:
+                raise ValueError("To filter on opening date you need to provide either tag code or code code")
+            return self._add_filter(
+                ContactByHcPartyTagCodeDateFilter(
+                    tag_type=None,
+                    tag_code=None,
+                    code_type=code_type,
+                    code_code=code_code,
+                    start_of_contact_opening_date=start_of_contact_opening_date,
+                    end_of_contact_opening_date=end_of_contact_opening_date,
+                    healthcare_party_id=self.__data_owner_id
+                ),
+                sort
+            )
+
+        def by_tag(
+            self,
+            tag_type: str,
+            tag_code: Optional[str],
+            start_of_contact_opening_date: Optional[int],
+            end_of_contact_opening_date: Optional[int],
+            sort: bool = False
+        ) -> 'ContactFilters.Builder':
+            """
+            Get all contacts that have a certain tag. If you specify only the tag type you will get all entities that
+            have at least a tag of that type. You can also limit the result to only contacts that are within a
+            certain opening date timeframe, but in that case you must specify the tag_code.
+            :param tag_type: a tag type
+            :param tag_code: a code for the provided tag type, or None if you want the filter to accept any entity
+            with a tag of the provided type.
+            :param start_of_contact_opening_date: if provided the filters accepts only contacts with an opening date
+            that is after this value (inclusive).
+            :param end_of_contact_opening_date: if provided the filters accepts only contacts with an opening date
+            that is before this value (inclusive).
+            :param sort: if True the data obtained through this filter will be sorted by tag type, tag code, and then
+            opening date
+            raises ValueError: if sort is True and another filter was previously set as the sort key, or if you provide
+            a range for the opening date but no tag code.
+            :return: self
+            """
+            if (start_of_contact_opening_date is not None or end_of_contact_opening_date is not None) and code_code is None:
+                raise ValueError("To filter on opening date you need to provide either tag code or code code")
+            return self._add_filter(
+                ContactByHcPartyTagCodeDateFilter(
+                    tag_type=tag_type,
+                    tag_code=tag_code,
+                    code_type=None,
+                    code_code=None,
+                    start_of_contact_opening_date=start_of_contact_opening_date,
+                    end_of_contact_opening_date=end_of_contact_opening_date,
+                    healthcare_party_id=self.__data_owner_id
+                ),
+                sort
+            )
+
+        def by_patients(
+            self,
+            patients_or_secret_ids: List[Union[Patient, str]],
+            sort: bool = False
+        ) -> 'ContactFilters.Builder':
+            """
+            Get all contacts linked to a group of patients.
+            :param patients_or_secret_ids: a list of patients or they secret ids. If you pass patients their secret ids
+            will be extracted automatically by this method.
+            :param sort: if true the data will be sorted by the patients, more specifically by the secret id of the
+            patient used to link the data to it.
+            :return: self
+            """
+            secret_ids = []
+            for x in patients_or_secret_ids:
+                if isinstance(x, str):
+                    secret_ids.append(x)
+                else:
+                    secret_ids.extend(self.__sdk.patient.get_secret_ids_of_blocking(x))
+            return self._add_filter(
+                ContactByHcPartyPatientTagCodeDateFilter(
+                    patient_secret_foreign_keys=secret_ids,
+                    healthcare_party_id=self.__data_owner_id
+                ),
+                sort
+            )
+
+        def with_service_ids(self, service_ids: List[str], sort: bool = False) -> 'ContactFilters.Builder':
+            """
+            Get all contacts with at least a service that has one of the provided ids.
+            :param service_ids: ids of services
+            :param sort: if True the output will be sorted in the same order as the provided service_ids. If a contact
+            has multiple services only the first matching service is considered
+            :return: self
+            """
+            return self._add_filter(
+                ContactByServiceIdsFilter(
+                    ids=service_ids
+                ),
+                sort
+            )
---
+++ python-wrapper/src/python/filters/PatientFilters.py
@@ -0,0 +1,285 @@
+from typing import List, Optional
+
+from icure import IcureSdk
+from icure.model import PatientAbstractFilter, ComplementFilter, PatientByIdsFilter, \
+    PatientByHcPartyAndIdentifiersFilter, PatientByHcPartyAndSsinsFilter, PatientByHcPartyAndSsinFilter, Identifier, \
+    PatientByHcPartyDateOfBirthBetweenFilter, Patient, PatientByHcPartyNameContainsFuzzyFilter, \
+    PatientByHcPartyGenderEducationProfession, PatientByHcPartyFilter, PatientByHcPartyAndActiveFilter, \
+    PatientByHcPartyAndAddressFilter, PatientByHcPartyAndExternalIdFilter, \
+    PatientByHcPartyAndTelecomFilter, PatientByHcPartyDateOfBirthFilter
+from icure.filters.AbstractFilterBuilder import AbstractFilterBuilder
+
+
+class PatientFilters:
+    """
+    Factory and builder for creating patient filters.
+    """
+
+    @classmethod
+    def complement_filter(cls, super_set: PatientAbstractFilter, sub_set: PatientAbstractFilter) -> PatientAbstractFilter:
+        """
+        :param super_set: a filter
+        :param sub_set: a filter
+        :return: A filter that provides all entities that are in super_set but not in subset. The result will preserve
+        the same ordering it had in the super_set.
+        """
+        return ComplementFilter(super_set=super_set, sub_set=sub_set)
+
+    @classmethod
+    def all_patients_for_self_filter(
+            cls,
+            sdk: IcureSdk
+    ) -> PatientAbstractFilter:
+        """
+        :param sdk: the sdk, used to get the id of the current data owner
+        :return: A filter to get all patients accessible to the current data owner
+        """
+        return PatientByHcPartyFilter(sdk.data_owner.get_current_data_owner_id_blocking())
+
+    @classmethod
+    def all_patients_for_data_owner_filter(
+            cls,
+            data_owner_id: str
+    ) -> PatientAbstractFilter:
+        """
+        :param data_owner_id: a data owner id. Note that you may need some special permissions to use the filter if the
+        data owner id is not the current data owner.
+        :return: A filter to get all patients accessible to the specified data owner
+        """
+        return PatientByHcPartyFilter(data_owner_id)
+
+    class Builder(AbstractFilterBuilder[PatientAbstractFilter, 'PatientFilters.Builder']):
+        """
+        Allows to build complex filters for patients
+        """
+
+        def __init__(self, sdk: IcureSdk, data_owner_id: Optional[str] = None):
+            """
+            Initializes the filter builder
+            :param sdk: the sdk, used to get some contextual information when needed.
+            :param data_owner_id: id of a data owner to use in the filter. For most filters only data that is accessible to
+            that data owner will be returned. If None will use the id of the data owner logged into the sdk.
+            """
+            self.__sdk = sdk
+            self.__data_owner_id = data_owner_id if data_owner_id is not None else sdk.data_owner.get_current_data_owner_id_blocking()
+
+        def by_ids(self, ids: List[str], sort: bool = False) -> 'PatientFilters.Builder':
+            """
+            The filter will return all patients with one of the provided ids. This filter ignores the data_owner_id of the
+            builder.
+            :param ids: a list of ids.
+            :param sort: if True the data obtained through this filter will be returned in the same order as the input ids.
+            :raises ValueError: if sort is True and another filter was previously set as the sort key.
+            :return: self
+            """
+            return self._add_filter(
+                PatientByIdsFilter(ids),
+                sort
+            )
+
+        def by_identifiers(self, identifiers: List[Identifier], sort: bool = False) -> 'PatientFilters.Builder':
+            """
+            The filter will return all the entities that have at least one of the specified identifiers. The system and
+            value of an identifier must match exactly for an entity to be returned by this filter (other properties
+            of the identifier are ignored).
+            :param identifiers: a list of identifiers
+            :param sort: if True the data obtained through this filter will be returned in the same order as the input
+            identifiers. In case an entity has multiple identifiers only the first matching identifier will be
+            considered.
+            :raises ValueError: if sort is True and another filter was previously set as the sort key.
+            :return: self
+            """
+            return self._add_filter(
+                PatientByHcPartyAndIdentifiersFilter(
+                    identifiers=identifiers,
+                    healthcare_party_id=self.__data_owner_id
+                ),
+                sort
+            )
+
+        def with_ssins(self, ssins: List[str], sort: bool = False) -> 'PatientFilters.Builder':
+            """
+            The filter will return all the patients where the ssin is one of the specified ones.
+            :param ssins: a list of ssins
+            :param sort: if True the data obtained through this filter will be returned in the same order as the input
+            ssins.
+            :raises ValueError: if sort is True and another filter was previously set as the sort key.
+            :return: self
+            """
+            if len(ssins) == 1:
+                return self._add_filter(
+                    PatientByHcPartyAndSsinFilter(
+                        ssin=ssins[0],
+                        healthcare_party_id=self.__data_owner_id
+                    ),
+                    sort
+                )
+            else:
+                return self._add_filter(
+                    PatientByHcPartyAndSsinsFilter(
+                        ssins=ssins.copy(),
+                        healthcare_party_id=self.__data_owner_id
+                    ),
+                    sort
+                )
+
+        def date_of_birth_between(self, from_date: int, to_date: int, sort: bool = False) -> 'PatientFilters.Builder':
+            """
+            Includes all the patients with date of birth between from_date and to_date.
+            :param from_date: the start date in YYYYMMDD format
+            :param to_date: the end date in YYYYMMDD format
+            :param sort: if true the data obtained through this filter result will be ordered by the patient date of birth.
+            :raises ValueError: if sort is True and another filter was previously set as the sort key
+            :return: self
+            """
+            if from_date == to_date:
+                return self._add_filter(
+                    PatientByHcPartyDateOfBirthBetweenFilter(
+                        min_date_of_birth=from_date,
+                        max_date_of_birth=to_date,
+                        healthcare_party_id=self.__data_owner_id
+                    ),
+                    sort
+                )
+            else:
+                return self._add_filter(
+                    PatientByHcPartyDateOfBirthFilter(
+                        date_of_birth=from_date,
+                        healthcare_party_id=self.__data_owner_id
+                    ),
+                    sort
+                )
+
+        def contains_fuzzy(self, search_string: str, sort: bool = False) -> 'PatientFilters.Builder':
+            """
+            Includes all the patients which first name, last name, maiden name or spouse name matches, even partially,
+            the search_string
+            :param search_string: part of a patient name
+            :param sort: if true the data obtained through this filter result will be ordered by the patient names
+            :raises ValueError: if sort is True and another filter was previously set as the sort key.
+            :return: self
+            """
+            return self._add_filter(
+                PatientByHcPartyNameContainsFuzzyFilter(
+                    search_string=search_string,
+                    healthcare_party_id=self.__data_owner_id
+                ),
+                sort
+            )
+
+        def by_gender_education_profession(self, gender: Patient.GenderEnum, education: Optional[str], profession: Optional[str], sort: bool = False) -> 'PatientFilters.Builder':
+            """
+            Includes all the patients which gender, education, and profession are the ones specified.
+            :param gender: the gender of the patients to include.
+            :param education: the education of the patients to include. If None, it will be ignored (matches any).
+            :param profession: the profession of the patients to include. If None, it will be ignored (matches any).
+            Must be None if education is None
+            :param sort: if true the data obtained through this filter will be ordered first by education then profession.
+            Must be false if both education and profession are not None.
+            :raises ValueError: if sort is True and another filter was previously set as the sort key, or if sort is True and
+            both education and profession have a value that is not None.
+            :return: self
+            """
+            if sort and education is not None and profession is not None:
+                raise ValueError("No need to sort if education and profession are both fixed values")
+            if profession is not None and education is None:
+                raise ValueError("If you want to specify profession you must also specify education")
+            return self._add_filter(
+                PatientByHcPartyGenderEducationProfession(
+                    gender=gender,
+                    education=education,
+                    profession=profession,
+                    healthcare_party_id=self.__data_owner_id
+                ),
+                sort
+            )
+
+        def is_active(self, active: bool) -> 'PatientFilters.Builder':
+            """
+            Filters all patients where the `active` field is set to the provided value
+            :param active:
+            :return: self
+            """
+            return self._add_filter(
+                PatientByHcPartyAndActiveFilter(
+                    active=active,
+                    healthcare_party_id=self.__data_owner_id
+                ),
+                False
+            )
+
+        def with_telecom(self, search_string: str, sort: bool = False) -> 'PatientFilters.Builder':
+            """
+            Finds all patient where there is at least an address with a telecom number (e.g. email or phone number) that
+            starts with the search_string. Ignores non-alphanumeric characters.
+            :param search_string: the beginning of the telecom number
+            :param sort: if True the resulting patients will be sorted lexicographically by telecom number. In case of
+            patients with multiple telecom numbers only the first matching number is considered.
+            :raises ValueError: if sort is True and another filter was previously set as the sort key
+            :return: self
+            """
+            return self._add_filter(
+                PatientByHcPartyAndTelecomFilter(
+                    search_string=search_string,
+                    healthcare_party_id=self.__data_owner_id
+                ),
+                sort
+            )
+
+        def with_address_postal_code_house_number(self, search_string: str, postal_code: str, house_number: Optional[str], sort: bool = False) -> 'PatientFilters.Builder':
+            """
+            Find all users with at least an address that matches the provided parameters.
+            :param search_string: a prefix of the normalized street+city to search for.
+            :param postal_code: a postal code prefix.
+            :param house_number: a house number prefix.
+            :param sort: if True the resulting patients will be sorted lexicographically first by street+city, then postal
+            code and finally house number. In case of patients with multiple address only the first matching address is
+            considered.
+            :raises ValueError: if sort is True and another filter was previously set as the sort key
+            :return: self
+            """
+            return self._add_filter(
+                PatientByHcPartyAndAddressFilter(
+                    search_string=search_string,
+                    postal_code=postal_code,
+                    house_number=house_number,
+                    healthcare_party_id=self.__data_owner_id
+                ),
+                sort
+            )
+
+        def with_address(self, search_string: str, sort: bool = False) -> 'PatientFilters.Builder':
+            """
+            Find all users with at least an address that matches the provided parameters.
+            :param search_string: a prefix of the normalized street+postal_code+city to search for.
+            :param sort: if True the resulting patients will be sorted lexicographically by street+postal_code+city. In case
+            of patients with multiple address only the first matching address is considered.
+            :raises ValueError: if sort is True and another filter was previously set as the sort key
+            :return: self
+            """
+            return self._add_filter(
+                PatientByHcPartyAndAddressFilter(
+                    search_string=search_string,
+                    postal_code=None,
+                    house_number=None,
+                    healthcare_party_id=self.__data_owner_id
+                ),
+                sort
+            )
+
+        def with_external_id(self, external_id: str, sort: bool=False) -> 'PatientFilters.Builder':
+            """
+            Find all patients with an `external_id` starting with the provided id
+            :param external_id: a prefix for the external id
+            :param sort: if True in case multiple patients start with the provided external id prefix the result will be
+            sorted by the lexicographic order of the full external id of each patient.
+            :raises ValueError: if sort is True and another filter was previously set as the sort key
+            :return: self
+            """
+            return self._add_filter(
+                PatientByHcPartyAndExternalIdFilter(
+                    external_id=external_id,
+                    healthcare_party_id=self.__data_owner_id
+                ),
+                sort
+            )
