0c44486a13a2a2788886323e44b33ec3b360315b
Luca Tremamunno <tremaluca96@gmail.com>
2024-07-19 18:05:53 +0200

    Update init

--- python-wrapper/src/python/__init__.py
+++ python-wrapper/src/python/__init__.py
@@ -1,1 +1,3 @@
 from icure.IcureSdk import IcureSdk
+from icure.CryptoStrategies import CryptoStrategies, KeyDataRecoveryRequest, RecoveredKeyData, ExportedKeyData, \
+    KeyGenerationRequestResult, KeyGenerationRequestResultAllow
