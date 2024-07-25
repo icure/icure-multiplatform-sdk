a1ed9f4521f48516f9ce70a7ceabecebfd313dc6
Luca Tremamunno <tremaluca96@gmail.com>
2024-07-25 17:53:55 +0200

    Add maintenance task doc

--- icure-sdk/src/commonMain/kotlin/com/icure/sdk/api/flavoured/MaintenanceTaskApi.kt
+++ icure-sdk/src/commonMain/kotlin/com/icure/sdk/api/flavoured/MaintenanceTaskApi.kt
@@ -1,8 +1,8 @@
 package com.icure.sdk.api.flavoured
 
 import com.icure.sdk.api.raw.RawMaintenanceTaskApi
+import com.icure.sdk.crypto.entities.CalendarItemShareOptions
 import com.icure.sdk.crypto.entities.MaintenanceTaskShareOptions
-import com.icure.sdk.crypto.entities.ShareMetadataBehaviour
 import com.icure.sdk.crypto.entities.SimpleShareResult
 import com.icure.sdk.crypto.entities.withTypeInfo
 import com.icure.sdk.model.DecryptedMaintenanceTask
@@ -36,16 +36,63 @@ import kotlinx.serialization.json.decodeFromJsonElement
 
 /* This interface includes the API calls that do not need encryption keys and do not return or consume encrypted/decrypted items, they are completely agnostic towards the presence of encrypted items */
 interface MaintenanceTaskBasicFlavourlessApi: Subscribable<MaintenanceTask, EncryptedMaintenanceTask> {
+	/**
+	 * Deletes a maintenance task. If you don't have write access to the maintenance task the method will fail.
+	 * @param entityId id of the maintenance task.
+	 * @return the id and revision of the deleted maintenance task.
+	 */
 	suspend fun deleteMaintenanceTask(entityId: String): DocIdentifier
+
+	/**
+	 * Deletes many maintenance tasks. Ids that do not correspond to an entity, or that correspond to an entity for which
+	 * you don't have write access will be ignored.
+	 * @param entityIds ids of the maintenance tasks.
+	 * @return the id and revision of the deleted maintenance tasks. If some entities could not be deleted (for example
+	 * because you had no write access to them) they will not be included in this list.
+	 */
 	suspend fun deleteMaintenanceTasks(entityIds: List<String>): List<DocIdentifier>
+	
+	/**
+	 * Get the ids of all maintenance tasks matching the provided filter.
+	 * @param filter a maintenance task filter
+	 * @return a list of maintenance task ids
+	 */
 	suspend fun matchMaintenanceTasksBy(filter: AbstractFilter<MaintenanceTask>): List<String>
 }
 
 /* This interface includes the API calls can be used on decrypted items if encryption keys are available *or* encrypted items if no encryption keys are available */
 interface MaintenanceTaskBasicFlavouredApi<E : MaintenanceTask> {
+	/**
+	 * Modifies a maintenance task. You need to have write access to the entity.
+	 * Flavoured method.
+	 * @param entity a maintenance task with update content
+	 * @return the maintenance task updated with the provided content and a new revision.
+	 */
 	suspend fun modifyMaintenanceTask(entity: E): E
+
+	/**
+	 * Get a maintenance task by its id. You must have read access to the entity. Fails if the id does not correspond to any
+	 * entity, corresponds to an entity that is not a maintenance task, or corresponds to an entity for which you don't have
+	 * read access.
+	 * Flavoured method.
+	 * @param entityId a maintenance task id.
+	 * @return the maintenance task with id [entityId].
+	 */
 	suspend fun getMaintenanceTask(entityId: String): E
+
+	/**
+	 * Get multiple maintenance tasks by their ids. Ignores all ids that do not correspond to an entity, correspond to
+	 * an entity that is not a maintenance task, or correspond to an entity for which you don't have read access.
+	 * Flavoured method.
+	 * @param entityIds a list of maintenance tasks ids
+	 * @return all maintenance tasks that you can access with one of the provided ids.
+	 */
 	suspend fun getMaintenanceTasks(entityIds: List<String>): List<E>
+
+	/**
+	 * @param filter a maintenance task filter
+	 * @return an iterator that iterates over all maintenance tasks matching the provided filter.
+	 */
 	suspend fun filterMaintenanceTasksBy(
 		filter: AbstractFilter<MaintenanceTask>
 	): PaginatedListIterator<E>
@@ -53,6 +100,17 @@ interface MaintenanceTaskBasicFlavouredApi<E : MaintenanceTask> {
 
 /* The extra API calls declared in this interface are the ones that can be used on encrypted or decrypted items but only when the user is a data owner */
 interface MaintenanceTaskFlavouredApi<E : MaintenanceTask> : MaintenanceTaskBasicFlavouredApi<E> {
+	/**
+	 * Share a maintenance task with another data owner. The maintenance task must already exist in the database for this method to
+	 * succeed. If you want to share the maintenance task before creation you should instead pass provide the delegates in
+	 * the initialise encryption metadata method.
+	 * @param delegateId the owner that will gain access to the maintenance task
+	 * @param maintenanceTask the maintenance task to share with [delegateId]
+	 * @param options specifies how the maintenance task will be shared. By default, all data available to the current user
+	 * will be shared, and the delegate will have the same permissions as the current user on the maintenance task. Refer
+	 * to the documentation of [CalendarItemShareOptions] for more information.
+	 * @return the updated maintenance task if the sharing was successful, or details on the errors if the sharing failed.
+	 */
 	suspend fun shareWith(
 		delegateId: String,
 		maintenanceTask: E,
@@ -60,20 +118,14 @@ interface MaintenanceTaskFlavouredApi<E : MaintenanceTask> : MaintenanceTaskBasi
 		options: MaintenanceTaskShareOptions? = null
 	): SimpleShareResult<E>
 
-
 	/**
-	 * Shares an existing access log with other data owners, allowing them to access the non-encrypted data of the access log and optionally also the
-	 * encrypted content, with read-only or read-write permissions.
-	 * @param maintenanceTask the [MaintenanceTask] to share.
-	 * @param delegates associates the id of data owners which will be granted access to the entity, to the following sharing options:
-	 * - shareEncryptionKey: specifies if the encryption key of the access log should be shared with the delegate, giving access to all encrypted
-	 * content of the entity, excluding other encrypted metadata (defaults to [ShareMetadataBehaviour.IfAvailable]).
-	 * - sharePatientId: specifies if the id of the patient that this access log refers to should be shared with the delegate. Normally this would
-	 * be the same as objectId, but it is encrypted separately from it allowing you to give access to the patient id without giving access to the other
-	 * encrypted data of the access log (defaults to [ShareMetadataBehaviour.IfAvailable]).
-	 * - requestedPermissions: the requested permissions for the delegate, defaults to [ShareMetadataBehaviour.IfAvailable].
-	 * @return the [SimpleShareResult] of the operation: the updated entity if the operation was successful or details of the error if
-	 * the operation failed.
+	 * Share a maintenance task with multiple data owners. The maintenance task must already exist in the database for this method to
+	 * succeed. If you want to share the maintenance task before creation you should instead pass provide the delegates in
+	 * the initialise encryption metadata method.
+	 * @param maintenanceTask the maintenance task to share
+	 * @param delegates specify the data owners which will gain access to the entity and the options for sharing with
+	 * each of them.
+	 * @return the updated maintenance task if the sharing was successful, or details on the errors if the sharing failed.
 	 */
 	suspend fun tryShareWithMany(
 		maintenanceTask: E,
@@ -81,18 +133,14 @@ interface MaintenanceTaskFlavouredApi<E : MaintenanceTask> : MaintenanceTaskBasi
 	): SimpleShareResult<E>
 
 	/**
-	 * Shares an existing access log with other data owners, allowing them to access the non-encrypted data of the access log and optionally also the
-	 * encrypted content, with read-only or read-write permissions.
-	 * @param maintenanceTask the [MaintenanceTask] to share.
-	 * @param delegates associates the id of data owners which will be granted access to the entity, to the following sharing options:
-	 * - shareEncryptionKey: specifies if the encryption key of the access log should be shared with the delegate, giving access to all encrypted
-	 * content of the entity, excluding other encrypted metadata (defaults to [ShareMetadataBehaviour.IfAvailable]).
-	 * - sharePatientId: specifies if the id of the patient that this access log refers to should be shared with the delegate. Normally this would
-	 * be the same as objectId, but it is encrypted separately from it allowing you to give access to the patient id without giving access to the other
-	 * encrypted data of the access log (defaults to [ShareMetadataBehaviour.IfAvailable]).
-	 * - requestedPermissions: the requested permissions for the delegate, defaults to [ShareMetadataBehaviour.IfAvailable].
-	 * @return the updated entity.
-	 * @throws IllegalStateException if the operation was not successful.
+	 * Share a maintenance task with multiple data owners. The maintenance task must already exist in the database for this method to
+	 * succeed. If you want to share the maintenance task before creation you should instead pass provide the delegates in
+	 * the initialise encryption metadata method.
+	 * Throws an exception if the operation fails.
+	 * @param maintenanceTask the maintenance task to share
+	 * @param delegates specify the data owners which will gain access to the entity and the options for sharing with
+	 * each of them.
+	 * @return the updated maintenance task.
 	 */
 	suspend fun shareWithMany(
 		maintenanceTask: E,
@@ -102,6 +150,12 @@ interface MaintenanceTaskFlavouredApi<E : MaintenanceTask> : MaintenanceTaskBasi
 
 /* The extra API calls declared in this interface are the ones that can only be used on decrypted items when encryption keys are available */
 interface MaintenanceTaskApi : MaintenanceTaskBasicFlavourlessApi, MaintenanceTaskFlavouredApi<DecryptedMaintenanceTask> {
+	/**
+	 * Create a new maintenance task. The provided maintenance task must have the encryption metadata initialised.
+	 * @param entity a maintenance task with initialised encryption metadata
+	 * @return the created maintenance task with updated revision.
+	 * @throws IllegalArgumentException if the encryption metadata of the input was not initialised.
+	 */
 	suspend fun createMaintenanceTask(entity: DecryptedMaintenanceTask): DecryptedMaintenanceTask
 
 	/**
@@ -120,14 +174,90 @@ interface MaintenanceTaskApi : MaintenanceTaskBasicFlavourlessApi, MaintenanceTa
 		@DefaultValue("emptyMap()")
 		delegates: Map<String, AccessLevel> = emptyMap(),
 	): DecryptedMaintenanceTask
+
+	/**
+	 * Attempts to extract the encryption keys of a maintenance task. If the user does not have access to any encryption key
+	 * of the access log the method will return an empty set.
+	 * Note: entities now have only one encryption key, but this method returns a set for compatibility with older
+	 * versions of iCure where this was not a guarantee.
+	 * @param maintenanceTask a maintenance task
+	 * @return the encryption keys extracted from the provided maintenance task.
+	 */
 	suspend fun getEncryptionKeysOf(maintenanceTask: MaintenanceTask): Set<HexString>
+
+	/**
+	 * Specifies if the current user has write access to a maintenance task.
+	 * @param maintenanceTask a maintenance task
+	 * @return if the current user has write access to the provided maintenance task
+	 */
 	suspend fun hasWriteAccess(maintenanceTask: MaintenanceTask): Boolean
+
+	/**
+	 * Attempts to extract the patient id linked to a maintenance task.
+	 * Note: maintenance tasks usually should be linked with only one patient, but this method returns a set for compatibility
+	 * with older versions of iCure
+	 * @param maintenanceTask a maintenance task
+	 * @return the id of the patient linked to the maintenance task, or empty if the current user can't access any patient id
+	 * of the maintenance task.
+	 */
 	suspend fun decryptPatientIdOf(maintenanceTask: MaintenanceTask): Set<String>
+
+	/**
+	 * Create metadata to allow other users to identify the anonymous delegates of a maintenance task.
+	 *
+	 * When calling this method the SDK will use all the information available to the current user to try to identify
+	 * any anonymous data-owners in the delegations of the provided maintenance task. The SDK will be able to identify the
+	 * anonymous data owners of the delegations only under the following conditions:
+	 * - The other participant of the delegation is the current data owner
+	 * - The SDK is using hierarchical data owners and the other participant of the delegation is a parent of the
+	 * current data owner
+	 * - There is de-anonymization metadata for the delegation shared with the current data owner.
+	 *
+	 * After identifying the anonymous delegates in the maintenance task the sdk will create the corresponding de-anonymization
+	 * metadata if it does not yet exist, and then share it with the provided delegates.
+	 *
+	 * Note that this delegation metadata may be used to de-anonymize the corresponding delegation in any MaintenanceTask,
+	 * not only in the provided instance.
+	 *
+	 * ## Example
+	 *
+	 * If you have a maintenance task E, and you have shared it with patient P and healthcare party H, H will not
+	 * be able to know that P has access to E until you create delegations de anonymization metadata and share that with
+	 * H. From now on, for any maintenance task that you have shared with P, H will be able to know that the maintenance task was
+	 * shared with P, regardless of whether it was created before or after the corresponding de-anonymization metadata.
+	 *
+	 * At the same time since the de-anonymization metadata applies to a specific delegation and therefore to a specific
+	 * delegator-delegate pair, you will not be able to see if P has access to a maintenance task that was created by H and
+	 * shared with you and P unless also H creates delegations de-anonymization metadata.
+	 *
+	 * @param entity a maintenance task
+	 * @param delegates a set of data owner ids
+	 */
 	suspend fun createDelegationDeAnonymizationMetadata(entity: MaintenanceTask, delegates: Set<String>)
+
+	/**
+	 * Decrypts a maintenance task, throwing an exception if it is not possible.
+	 * @param maintenanceTask a maintenance task
+	 * @return the decrypted maintenance task
+	 * @throws EntityEncryptionException if the maintenance task could not be decrypted
+	 */
 	suspend fun decrypt(maintenanceTask: EncryptedMaintenanceTask): DecryptedMaintenanceTask
+
+	/**
+	 * Tries to decrypt a maintenance task, returns the input if it is not possible.
+	 * @param maintenanceTask an encrypted maintenance task
+	 * @return the decrypted maintenance task if the decryption was successful or the input if it was not.
+	 */
 	suspend fun tryDecrypt(maintenanceTask: EncryptedMaintenanceTask): MaintenanceTask
 
+	/**
+	 * Give access to the encrypted flavour of the api
+	 */
 	val encrypted: MaintenanceTaskFlavouredApi<EncryptedMaintenanceTask>
+
+	/**
+	 * Gives access to the polymorphic flavour of the api
+	 */
 	val tryAndRecover: MaintenanceTaskFlavouredApi<MaintenanceTask>
 }
 
