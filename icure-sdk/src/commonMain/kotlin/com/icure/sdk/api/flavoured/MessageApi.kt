6dd6df97b075f4124f1ea20daff9203e40e83d86
Luca Tremamunno <tremaluca96@gmail.com>
2024-07-26 16:33:05 +0200

    Add message documentation

--- icure-sdk/src/commonMain/kotlin/com/icure/sdk/api/flavoured/MessageApi.kt
+++ icure-sdk/src/commonMain/kotlin/com/icure/sdk/api/flavoured/MessageApi.kt
@@ -3,7 +3,6 @@ package com.icure.sdk.api.flavoured
 import com.icure.sdk.api.raw.RawMessageApi
 import com.icure.sdk.crypto.entities.MessageShareOptions
 import com.icure.sdk.crypto.entities.SecretIdOption
-import com.icure.sdk.crypto.entities.ShareMetadataBehaviour
 import com.icure.sdk.crypto.entities.SimpleShareResult
 import com.icure.sdk.crypto.entities.withTypeInfo
 import com.icure.sdk.model.DecryptedMessage
@@ -41,24 +40,84 @@ import kotlinx.serialization.json.decodeFromJsonElement
 
 /* This interface includes the API calls that do not need encryption keys and do not return or consume encrypted/decrypted items, they are completely agnostic towards the presence of encrypted items */
 interface MessageBasicFlavourlessApi: Subscribable<Message, EncryptedMessage> {
+	/**
+	 * Get the ids of all health elements matching the provided filter.
+	 * @param filter a health element filter
+	 * @return a list of health element ids
+	 */
 	suspend fun matchMessagesBy(filter: AbstractFilter<Message>): List<String>
+
+	/**
+	 * Deletes a message. If you don't have write access to the Message the method will fail.
+	 * @param entityId id of the Message.
+	 * @return the id and revision of the deleted Message.
+	 */
 	suspend fun deleteMessage(entityId: String): DocIdentifier
+
+	/**
+	 * Deletes many messages. Ids that do not correspond to an entity, or that correspond to an entity for which
+	 * you don't have write access will be ignored.
+	 * @param entityIds ids of the messages.
+	 * @return the id and revision of the deleted messages. If some entities could not be deleted (for example
+	 * because you had no write access to them) they will not be included in this list.
+	 */
 	suspend fun deleteMessages(entityIds: List<String>): List<DocIdentifier>
 }
 
 /* This interface includes the API calls can be used on decrypted items if encryption keys are available *or* encrypted items if no encryption keys are available */
 interface MessageBasicFlavouredApi<E : Message> {
+	/**
+	 * Modifies a message. You need to have write access to the entity.
+	 * Flavoured method.
+	 * @param entity a message with update content
+	 * @return the Message updated with the provided content and a new revision.
+	 */
 	suspend fun modifyMessage(entity: E): E
+
+	/**
+	 * Get a message by its id. You must have read access to the entity. Fails if the id does not correspond to any
+	 * entity, corresponds to an entity that is not a message, or corresponds to an entity for which you don't have
+	 * read access.
+	 * Flavoured method.
+	 * @param entityId a message id.
+	 * @return the Message with id [entityId].
+	 */
 	suspend fun getMessage(entityId: String): E
+
+	/**
+	 * Get multiple messages by their ids. Ignores all ids that do not correspond to an entity, correspond to
+	 * an entity that is not a message, or correspond to an entity for which you don't have read access.
+	 * Flavoured method.
+	 * @param entityIds a list of messages ids
+	 * @return all messages that you can access with one of the provided ids.
+	 */
 	suspend fun getMessages(entityIds: List<String>): List<E>
+
+	/**
+	 * @param filter a message filter
+	 * @return an iterator that iterates over all messages matching the provided filter.
+	 */
 	suspend fun filterMessagesBy(filter: AbstractFilter<Message>): PaginatedListIterator<E>
 
+	@Deprecated("Will be replaced by filter")
 	suspend fun listMessagesByTransportGuids(hcPartyId: String, transportGuids: List<String>): List<E>
+
+	@Deprecated("Will be replaced by filter")
 	suspend fun findMessagesByHCPartyPatientForeignKeys(secretPatientKeys: List<String>): List<E>
+
+	@Deprecated("Will be replaced by filter")
 	suspend fun findMessages(startKey: JsonElement?, startDocumentId: String?, limit: Int?): PaginatedList<E>
+
+	@Deprecated("Will be replaced by filter")
 	suspend fun getChildrenMessages(messageId: String): List<E>
+
+	@Deprecated("Will be replaced by filter")
 	suspend fun getMessagesChildren(messageIds: List<String>): List<E>
+
+	@Deprecated("Will be replaced by filter")
 	suspend fun listMessagesByInvoices(invoiceIds: List<String>): List<E>
+
+	@Deprecated("Will be replaced by filter")
 	suspend fun findMessagesByTransportGuid(transportGuid: String): PaginatedList<E>
 
 	// TODO: Implement filter for this method
@@ -90,12 +149,35 @@ interface MessageBasicFlavouredApi<E : Message> {
 		limit: Int?,
     ): PaginatedList<E>
 
+	@Deprecated("Status bits have unclear meaning, use read status instead")
 	suspend fun setMessagesStatusBits(entityIds: List<String>, statusBits: Int): List<E>
+
+	/**
+	 * Updates the read status of the messages with the provided id. You can use this method even if you don't have write permissions
+	 * on the message, as long as you have read permissions.
+	 * @param entityIds the ids of the messages to update
+	 * @param time the unix timestamp of when the message was read
+	 * @param readStatus if the message was read
+	 * @param userId the id of the user for which the status should be updated
+	 * @return the updated messages.
+	 */
+	// TODO improve security by making that by default a user can only update his own information unless they have special permissions
 	suspend fun setMessagesReadStatus(entityIds: List<String>, time: Long?, readStatus: Boolean, userId: String): List<E>
 }
 
 /* The extra API calls declared in this interface are the ones that can be used on encrypted or decrypted items but only when the user is a data owner */
 interface MessageFlavouredApi<E : Message> : MessageBasicFlavouredApi<E> {
+	/**
+	 * Share a message with another data owner. The Message must already exist in the database for this method to
+	 * succeed. If you want to share the Message before creation you should instead pass provide the delegates in
+	 * the initialise encryption metadata method.
+	 * @param delegateId the owner that will gain access to the Message
+	 * @param healthElement the Message to share with [delegateId]
+	 * @param options specifies how the Message will be shared. By default, all data available to the current user
+	 * will be shared, and the delegate will have the same permissions as the current user on the Message. Refer
+	 * to the documentation of [MessageShareOptions] for more information.
+	 * @return the updated Message if the sharing was successful, or details on the errors if the sharing failed.
+	 */
 	suspend fun shareWith(
 		delegateId: String,
 		message: E,
@@ -103,18 +185,13 @@ interface MessageFlavouredApi<E : Message> : MessageBasicFlavouredApi<E> {
 	): SimpleShareResult<E>
 
 	/**
-	 * Shares an existing access log with other data owners, allowing them to access the non-encrypted data of the access log and optionally also the
-	 * encrypted content, with read-only or read-write permissions.
-	 * @param message the [Message] to share.
-	 * @param delegates associates the id of data owners which will be granted access to the entity, to the following sharing options:
-	 * - shareEncryptionKey: specifies if the encryption key of the access log should be shared with the delegate, giving access to all encrypted
-	 * content of the entity, excluding other encrypted metadata (defaults to [ShareMetadataBehaviour.IfAvailable]).
-	 * - sharePatientId: specifies if the id of the patient that this access log refers to should be shared with the delegate. Normally this would
-	 * be the same as objectId, but it is encrypted separately from it allowing you to give access to the patient id without giving access to the other
-	 * encrypted data of the access log (defaults to [ShareMetadataBehaviour.IfAvailable]).
-	 * - requestedPermissions: the requested permissions for the delegate, defaults to [ShareMetadataBehaviour.IfAvailable].
-	 * @return the [SimpleShareResult] of the operation: the updated entity if the operation was successful or details of the error if
-	 * the operation failed.
+	 * Share a message with multiple data owners. The Message must already exist in the database for this method to
+	 * succeed. If you want to share the Message before creation you should instead pass provide the delegates in
+	 * the initialise encryption metadata method.
+	 * @param healthElement the Message to share
+	 * @param delegates specify the data owners which will gain access to the entity and the options for sharing with
+	 * each of them.
+	 * @return the updated Message if the sharing was successful, or details on the errors if the sharing failed.
 	 */
 	suspend fun tryShareWithMany(
 		message: E,
@@ -122,24 +199,21 @@ interface MessageFlavouredApi<E : Message> : MessageBasicFlavouredApi<E> {
 	): SimpleShareResult<E>
 
 	/**
-	 * Shares an existing access log with other data owners, allowing them to access the non-encrypted data of the access log and optionally also the
-	 * encrypted content, with read-only or read-write permissions.
-	 * @param message the [Message] to share.
-	 * @param delegates associates the id of data owners which will be granted access to the entity, to the following sharing options:
-	 * - shareEncryptionKey: specifies if the encryption key of the access log should be shared with the delegate, giving access to all encrypted
-	 * content of the entity, excluding other encrypted metadata (defaults to [ShareMetadataBehaviour.IfAvailable]).
-	 * - sharePatientId: specifies if the id of the patient that this access log refers to should be shared with the delegate. Normally this would
-	 * be the same as objectId, but it is encrypted separately from it allowing you to give access to the patient id without giving access to the other
-	 * encrypted data of the access log (defaults to [ShareMetadataBehaviour.IfAvailable]).
-	 * - requestedPermissions: the requested permissions for the delegate, defaults to [ShareMetadataBehaviour.IfAvailable].
-	 * @return the updated entity.
-	 * @throws IllegalStateException if the operation was not successful.
+	 * Share a message with multiple data owners. The Message must already exist in the database for this method to
+	 * succeed. If you want to share the Message before creation you should instead pass provide the delegates in
+	 * the initialise encryption metadata method.
+	 * Throws an exception if the operation fails.
+	 * @param healthElement the Message to share
+	 * @param delegates specify the data owners which will gain access to the entity and the options for sharing with
+	 * each of them.
+	 * @return the updated Message.
 	 */
 	suspend fun shareWithMany(
 		message: E,
 		delegates: Map<String, MessageShareOptions>
 	): E
 
+	@Deprecated("Will be replaced by filter")
 	suspend fun findMessagesByHcPartyPatient(
 		hcPartyId: String,
 		patient: Patient,
@@ -154,7 +228,38 @@ interface MessageFlavouredApi<E : Message> : MessageBasicFlavouredApi<E> {
 
 /* The extra API calls declared in this interface are the ones that can only be used on decrypted items when encryption keys are available */
 interface MessageApi : MessageBasicFlavourlessApi, MessageFlavouredApi<DecryptedMessage> {
+	/**
+	 * Create a new Message. The provided Message must have the encryption metadata initialised. This method requires
+	 * the permission to create messages outside of topics. If you want to create a message within a topic use the
+	 * [createMessageInTopic] method instead.
+	 * @param entity a message with initialised encryption metadata
+	 * @return the created Message with updated revision.
+	 * @throws IllegalArgumentException if the encryption metadata of the input was not initialised.
+	 */
 	suspend fun createMessage(entity: DecryptedMessage): DecryptedMessage
+
+	/**
+	 * Create a new Message. The provided Message must have the encryption metadata initialised, and the id of the topic
+	 * set in [Message.transportGuid] (note that your configuration must not encrypt the transport guid). The user needs
+	 * to be a participant in that topic for this method to succeed.
+	 * @param entity a message with initialised encryption metadata and with a transportGuid set to the topic
+	 * @return the created Message with updated revision.
+	 * @throws IllegalArgumentException if the encryption metadata of the input was not initialised.
+	 */
+	suspend fun createMessageInTopic(entity: DecryptedMessage): DecryptedMessage
+
+	/**
+	 * Creates a new Message with initialised encryption metadata
+	 * @param base a message with initialised content and uninitialised encryption metadata. The result of this
+	 * method takes the content from [base] if provided.
+	 * @param patient the patient linked to the Message.
+	 * @param user the current user, will be used for the auto-delegations if provided.
+	 * @param delegates additional data owners that will have access to the newly created entity. You may choose the
+	 * permissions that the delegates will have on the entity, but they will have access to all encryption metadata.
+	 * @param secretId specifies which secret id of [patient] to use for the new Message
+	 * @return a message with initialised encryption metadata.
+	 * @throws IllegalArgumentException if base is not null and has a revision or has encryption metadata.
+	 */
 	suspend fun withEncryptionMetadata(
 		base: DecryptedMessage?,
 		patient: Patient?,
@@ -165,16 +270,91 @@ interface MessageApi : MessageBasicFlavourlessApi, MessageFlavouredApi<Decrypted
 		@DefaultValue("com.icure.sdk.crypto.entities.SecretIdOption.UseAnySharedWithParent")
 		secretId: SecretIdOption = SecretIdOption.UseAnySharedWithParent,
 	): DecryptedMessage
-	suspend fun getEncryptionKeysOf(message: Message): Set<HexString>
-	suspend fun hasWriteAccess(message: Message): Boolean
-	suspend fun decryptPatientIdOf(message: Message): Set<String>
-	suspend fun createDelegationDeAnonymizationMetadata(entity: Message, delegates: Set<String>)
-	suspend fun decrypt(message: EncryptedMessage): DecryptedMessage
-	suspend fun tryDecrypt(message: EncryptedMessage): Message
 
+	/**
+	 * Attempts to extract the encryption keys of a message. If the user does not have access to any encryption key
+	 * of the access log the method will return an empty set.
+	 * Note: entities now have only one encryption key, but this method returns a set for compatibility with older
+	 * versions of iCure where this was not a guarantee.
+	 * @param healthElement a message
+	 * @return the encryption keys extracted from the provided Message.
+	 */
+	suspend fun getEncryptionKeysOf(message: Message): Set<HexString>
+
+	/**
+	 * Specifies if the current user has write access to a message.
+	 * @param healthElement a message
+	 * @return if the current user has write access to the provided Message
+	 */
+	suspend fun hasWriteAccess(message: Message): Boolean
+
+	/**
+	 * Attempts to extract the patient id linked to a message.
+	 * Note: messages usually should be linked with only one patient, but this method returns a set for compatibility
+	 * with older versions of iCure
+	 * @param healthElement a message
+	 * @return the id of the patient linked to the Message, or empty if the current user can't access any patient id
+	 * of the Message.
+	 */
+	suspend fun decryptPatientIdOf(message: Message): Set<String>
+
+	/**
+	 * Create metadata to allow other users to identify the anonymous delegates of a message.
+	 *
+	 * When calling this method the SDK will use all the information available to the current user to try to identify
+	 * any anonymous data-owners in the delegations of the provided Message. The SDK will be able to identify the
+	 * anonymous data owners of the delegations only under the following conditions:
+	 * - The other participant of the delegation is the current data owner
+	 * - The SDK is using hierarchical data owners and the other participant of the delegation is a parent of the
+	 * current data owner
+	 * - There is de-anonymization metadata for the delegation shared with the current data owner.
+	 *
+	 * After identifying the anonymous delegates in the Message the sdk will create the corresponding de-anonymization
+	 * metadata if it does not yet exist, and then share it with the provided delegates.
+	 *
+	 * Note that this delegation metadata may be used to de-anonymize the corresponding delegation in any Message,
+	 * not only in the provided instance.
+	 *
+	 * ## Example
+	 *
+	 * If you have a message E, and you have shared it with patient P and healthcare party H, H will not
+	 * be able to know that P has access to E until you create delegations de anonymization metadata and share that with
+	 * H. From now on, for any Message that you have shared with P, H will be able to know that the Message was
+	 * shared with P, regardless of whether it was created before or after the corresponding de-anonymization metadata.
+	 *
+	 * At the same time since the de-anonymization metadata applies to a specific delegation and therefore to a specific
+	 * delegator-delegate pair, you will not be able to see if P has access to a message that was created by H and
+	 * shared with you and P unless also H creates delegations de-anonymization metadata.
+	 *
+	 * @param entity a message
+	 * @param delegates a set of data owner ids
+	 */
+	suspend fun createDelegationDeAnonymizationMetadata(entity: Message, delegates: Set<String>)
+
+	/**
+	 * Decrypts a message, throwing an exception if it is not possible.
+	 * @param healthElement a message
+	 * @return the decrypted Message
+	 * @throws EntityEncryptionException if the Message could not be decrypted
+	 */
+	suspend fun decrypt(message: EncryptedMessage): DecryptedMessage
+
+	/**
+	 * Tries to decrypt a message, returns the input if it is not possible.
+	 * @param healthElement an encrypted Message
+	 * @return the decrypted Message if the decryption was successful or the input if it was not.
+	 */
+	suspend fun tryDecrypt(message: EncryptedMessage): Message
+	
+	/**
+	 * Give access to the encrypted flavour of the api
+	 */
 	val encrypted: MessageFlavouredApi<EncryptedMessage>
+
+	/**
+	 * Gives access to the polymorphic flavour of the api
+	 */
 	val tryAndRecover: MessageFlavouredApi<Message>
-	suspend fun createMessageInTopic(entity: DecryptedMessage): DecryptedMessage
 }
 
 interface MessageBasicApi : MessageBasicFlavourlessApi, MessageBasicFlavouredApi<EncryptedMessage>
