02beb03359b9871aa504c3ecc0d069cc4f11c1e1
Luca Tremamunno <tremaluca96@gmail.com>
2024-07-26 18:07:29 +0200

    Add topic doc

--- icure-sdk/src/commonMain/kotlin/com/icure/sdk/api/flavoured/TopicApi.kt
+++ icure-sdk/src/commonMain/kotlin/com/icure/sdk/api/flavoured/TopicApi.kt
@@ -41,24 +41,96 @@ import kotlinx.serialization.json.decodeFromJsonElement
 
 /* This interface includes the API calls that do not need encryption keys and do not return or consume encrypted/decrypted items, they are completely agnostic towards the presence of encrypted items */
 interface TopicBasicFlavourlessApi : Subscribable<Topic, EncryptedTopic> {
+
+	/**
+	 * Deletes a topic. If you don't have write access to the topic the method will fail.
+	 * @param entityId id of the topic.
+	 * @return the id and revision of the deleted topic.
+	 */
 	suspend fun deleteTopic(entityId: String): DocIdentifier
+
+	/**
+	 * Deletes many topics. Ids that do not correspond to an entity, or that correspond to an entity for which
+	 * you don't have write access will be ignored.
+	 * @param entityIds ids of the topics.
+	 * @return the id and revision of the deleted topics. If some entities could not be deleted (for example
+	 * because you had no write access to them) they will not be included in this list.
+	 */
 	suspend fun deleteTopics(entityIds: List<String>): List<DocIdentifier>
+	/**
+	 * Get the ids of all health elements matching the provided filter.
+	 * @param filter a health element filter
+	 * @return a list of health element ids
+	 */
 	suspend fun matchTopicsBy(filter: AbstractFilter<Topic>): List<String>
 }
 
 /* This interface includes the API calls can be used on decrypted items if encryption keys are available *or* encrypted items if no encryption keys are available */
 interface TopicBasicFlavouredApi<E : Topic> {
+	/**
+	 * Modifies a topic. You need to have write access to the entity.
+	 * Flavoured method.
+	 * @param entity a topic with update content
+	 * @return the topic updated with the provided content and a new revision.
+	 */
 	suspend fun modifyTopic(entity: E): E
+
+	/**
+	 * Get a topic by its id. You must have read access to the entity. Fails if the id does not correspond to any
+	 * entity, corresponds to an entity that is not a topic, or corresponds to an entity for which you don't have
+	 * read access.
+	 * Flavoured method.
+	 * @param entityId a topic id.
+	 * @return the topic with id [entityId].
+	 */
 	suspend fun getTopic(entityId: String): E
+
+	/**
+	 * Get multiple topics by their ids. Ignores all ids that do not correspond to an entity, correspond to
+	 * an entity that is not a topic, or correspond to an entity for which you don't have read access.
+	 * Flavoured method.
+	 * @param entityIds a list of topics ids
+	 * @return all topics that you can access with one of the provided ids.
+	 */
 	suspend fun getTopics(entityIds: List<String>): List<E>
+
+	/**
+	 * @param filter a topic filter
+	 * @return an iterator that iterates over all topics matching the provided filter.
+	 */
 	suspend fun filterTopicsBy(filter: AbstractFilter<Topic>): PaginatedListIterator<E>
 
+	/**
+	 * Add a participant to the topic. The participant will be able to create messages associated to the topic.
+	 * @param entityId id of the topic
+	 * @param dataOwnerId id of the new participant
+	 * @param topicRole the role that the participant will have in the topic
+	 * @return the updated topic
+	 */
 	suspend fun addParticipant(entityId: String, dataOwnerId: String, topicRole: TopicRole): E
+
+	/**
+	 * Removes a participant from a topic. The participant will not be able anymore to search for messages of that topic.
+	 * @param entityId id of the topic
+	 * @param dataOwnerId id of the participant to remove
+	 * @return the updated topic
+	 */
 	suspend fun removeParticipant(entityId: String, dataOwnerId: String): E
 }
 
 /* The extra API calls declared in this interface are the ones that can be used on encrypted or decrypted items but only when the user is a data owner */
 interface TopicFlavouredApi<E : Topic> : TopicBasicFlavouredApi<E> {
+	/**
+	 * Share a topic with another data owner. The topic must already exist in the database for this method to
+	 * succeed. If you want to share the topic before creation you should instead pass provide the delegates in
+	 * the initialise encryption metadata method.
+	 * @param delegateId the owner that will gain access to the topic
+	 * @param healthElement the topic to share with [delegateId]
+	 * @param options specifies how the topic will be shared. By default, all data available to the current user
+	 * will be shared, and the delegate will have the same permissions as the current user on the topic. Refer
+	 * to the documentation of [TopicShareOptions] for more information.
+	 * @return the updated topic if the sharing was successful, or details on the errors if the sharing failed.
+	 */
 	suspend fun shareWith(
 		delegateId: String,
 		topic: E,
@@ -67,18 +139,13 @@ interface TopicFlavouredApi<E : Topic> : TopicBasicFlavouredApi<E> {
 	): SimpleShareResult<E>
 
 	/**
-	 * Shares an existing access log with other data owners, allowing them to access the non-encrypted data of the access log and optionally also the
-	 * encrypted content, with read-only or read-write permissions.
-	 * @param topic the [Topic] to share.
-	 * @param delegates associates the id of data owners which will be granted access to the entity, to the following sharing options:
-	 * - shareEncryptionKey: specifies if the encryption key of the access log should be shared with the delegate, giving access to all encrypted
-	 * content of the entity, excluding other encrypted metadata (defaults to [ShareMetadataBehaviour.IfAvailable]).
-	 * - sharePatientId: specifies if the id of the patient that this access log refers to should be shared with the delegate. Normally this would
-	 * be the same as objectId, but it is encrypted separately from it allowing you to give access to the patient id without giving access to the other
-	 * encrypted data of the access log (defaults to [ShareMetadataBehaviour.IfAvailable]).
-	 * - requestedPermissions: the requested permissions for the delegate, defaults to [ShareMetadataBehaviour.IfAvailable].
-	 * @return the [SimpleShareResult] of the operation: the updated entity if the operation was successful or details of the error if
-	 * the operation failed.
+	 * Share a topic with multiple data owners. The topic must already exist in the database for this method to
+	 * succeed. If you want to share the topic before creation you should instead pass provide the delegates in
+	 * the initialise encryption metadata method.
+	 * @param healthElement the topic to share
+	 * @param delegates specify the data owners which will gain access to the entity and the options for sharing with
+	 * each of them.
+	 * @return the updated topic if the sharing was successful, or details on the errors if the sharing failed.
 	 */
 	suspend fun tryShareWithMany(
 		topic: E,
@@ -86,18 +153,14 @@ interface TopicFlavouredApi<E : Topic> : TopicBasicFlavouredApi<E> {
 	): SimpleShareResult<E>
 
 	/**
-	 * Shares an existing access log with other data owners, allowing them to access the non-encrypted data of the access log and optionally also the
-	 * encrypted content, with read-only or read-write permissions.
-	 * @param topic the [Topic] to share.
-	 * @param delegates associates the id of data owners which will be granted access to the entity, to the following sharing options:
-	 * - shareEncryptionKey: specifies if the encryption key of the access log should be shared with the delegate, giving access to all encrypted
-	 * content of the entity, excluding other encrypted metadata (defaults to [ShareMetadataBehaviour.IfAvailable]).
-	 * - sharePatientId: specifies if the id of the patient that this access log refers to should be shared with the delegate. Normally this would
-	 * be the same as objectId, but it is encrypted separately from it allowing you to give access to the patient id without giving access to the other
-	 * encrypted data of the access log (defaults to [ShareMetadataBehaviour.IfAvailable]).
-	 * - requestedPermissions: the requested permissions for the delegate, defaults to [ShareMetadataBehaviour.IfAvailable].
-	 * @return the updated entity.
-	 * @throws IllegalStateException if the operation was not successful.
+	 * Share a topic with multiple data owners. The topic must already exist in the database for this method to
+	 * succeed. If you want to share the topic before creation you should instead pass provide the delegates in
+	 * the initialise encryption metadata method.
+	 * Throws an exception if the operation fails.
+	 * @param healthElement the topic to share
+	 * @param delegates specify the data owners which will gain access to the entity and the options for sharing with
+	 * each of them.
+	 * @return the updated topic.
 	 */
 	suspend fun shareWithMany(
 		topic: E,
@@ -107,7 +170,26 @@ interface TopicFlavouredApi<E : Topic> : TopicBasicFlavouredApi<E> {
 
 /* The extra API calls declared in this interface are the ones that can only be used on decrypted items when encryption keys are available */
 interface TopicApi : TopicBasicFlavourlessApi, TopicFlavouredApi<DecryptedTopic> {
+	/**
+	 * Create a new topic. The provided topic must have the encryption metadata initialised.
+	 * @param entity a topic with initialised encryption metadata
+	 * @return the created topic with updated revision.
+	 * @throws IllegalArgumentException if the encryption metadata of the input was not initialised.
+	 */
 	suspend fun createTopic(entity: DecryptedTopic): DecryptedTopic
+
+	/**
+	 * Creates a new topic with initialised encryption metadata
+	 * @param base a topic with initialised content and uninitialised encryption metadata. The result of this
+	 * method takes the content from [base] if provided.
+	 * @param patient the patient linked to the topic.
+	 * @param user the current user, will be used for the auto-delegations if provided.
+	 * @param delegates additional data owners that will have access to the newly created entity. You may choose the
+	 * permissions that the delegates will have on the entity, but they will have access to all encryption metadata.
+	 * @param secretId specifies which secret id of [patient] to use for the new topic
+	 * @return a topic with initialised encryption metadata.
+	 * @throws IllegalArgumentException if base is not null and has a revision or has encryption metadata.
+	 */
 	suspend fun withEncryptionMetadata(
 		base: DecryptedTopic?,
 		patient: Patient?,
@@ -118,14 +200,90 @@ interface TopicApi : TopicBasicFlavourlessApi, TopicFlavouredApi<DecryptedTopic>
 		@DefaultValue("com.icure.sdk.crypto.entities.SecretIdOption.UseAnySharedWithParent")
 		secretId: SecretIdOption = SecretIdOption.UseAnySharedWithParent,
 	): DecryptedTopic
-	suspend fun getEncryptionKeysOf(topic: Topic): Set<HexString>
-	suspend fun hasWriteAccess(topic: Topic): Boolean
-	suspend fun decryptPatientIdOf(topic: Topic): Set<String>
-	suspend fun createDelegationDeAnonymizationMetadata(entity: Topic, delegates: Set<String>)
-	suspend fun decrypt(topic: EncryptedTopic): DecryptedTopic
-	suspend fun tryDecrypt(topic: EncryptedTopic): Topic
 
+	/**
+	 * Attempts to extract the encryption keys of a topic. If the user does not have access to any encryption key
+	 * of the access log the method will return an empty set.
+	 * Note: entities now have only one encryption key, but this method returns a set for compatibility with older
+	 * versions of iCure where this was not a guarantee.
+	 * @param healthElement a topic
+	 * @return the encryption keys extracted from the provided topic.
+	 */
+	suspend fun getEncryptionKeysOf(topic: Topic): Set<HexString>
+
+	/**
+	 * Specifies if the current user has write access to a topic.
+	 * @param healthElement a topic
+	 * @return if the current user has write access to the provided topic
+	 */
+	suspend fun hasWriteAccess(topic: Topic): Boolean
+
+	/**
+	 * Attempts to extract the patient id linked to a topic.
+	 * Note: topics usually should be linked with only one patient, but this method returns a set for compatibility
+	 * with older versions of iCure
+	 * @param healthElement a topic
+	 * @return the id of the patient linked to the topic, or empty if the current user can't access any patient id
+	 * of the topic.
+	 */
+	suspend fun decryptPatientIdOf(topic: Topic): Set<String>
+
+	/**
+	 * Create metadata to allow other users to identify the anonymous delegates of a topic.
+	 *
+	 * When calling this method the SDK will use all the information available to the current user to try to identify
+	 * any anonymous data-owners in the delegations of the provided topic. The SDK will be able to identify the
+	 * anonymous data owners of the delegations only under the following conditions:
+	 * - The other participant of the delegation is the current data owner
+	 * - The SDK is using hierarchical data owners and the other participant of the delegation is a parent of the
+	 * current data owner
+	 * - There is de-anonymization metadata for the delegation shared with the current data owner.
+	 *
+	 * After identifying the anonymous delegates in the topic the sdk will create the corresponding de-anonymization
+	 * metadata if it does not yet exist, and then share it with the provided delegates.
+	 *
+	 * Note that this delegation metadata may be used to de-anonymize the corresponding delegation in any Topic,
+	 * not only in the provided instance.
+	 *
+	 * ## Example
+	 *
+	 * If you have a topic E, and you have shared it with patient P and healthcare party H, H will not
+	 * be able to know that P has access to E until you create delegations de anonymization metadata and share that with
+	 * H. From now on, for any topic that you have shared with P, H will be able to know that the topic was
+	 * shared with P, regardless of whether it was created before or after the corresponding de-anonymization metadata.
+	 *
+	 * At the same time since the de-anonymization metadata applies to a specific delegation and therefore to a specific
+	 * delegator-delegate pair, you will not be able to see if P has access to a topic that was created by H and
+	 * shared with you and P unless also H creates delegations de-anonymization metadata.
+	 *
+	 * @param entity a topic
+	 * @param delegates a set of data owner ids
+	 */
+	suspend fun createDelegationDeAnonymizationMetadata(entity: Topic, delegates: Set<String>)
+
+	/**
+	 * Decrypts a topic, throwing an exception if it is not possible.
+	 * @param healthElement a topic
+	 * @return the decrypted topic
+	 * @throws EntityEncryptionExcep
+	 */
+	suspend fun decrypt(topic: EncryptedTopic): DecryptedTopic
+
+	/**
+	 * Tries to decrypt a topic, returns the input if it is not possible.
+	 * @param healthElement an encrypted topic
+	 * @return the decrypted topic if the decryption was successful or the input if it was not.
+	 */
+	suspend fun tryDecrypt(topic: EncryptedTopic): Topic
+	
+	/**
+	 * Give access to the encrypted flavour of the api
+	 */
 	val encrypted: TopicFlavouredApi<EncryptedTopic>
+
+	/**
+	 * Gives access to the polymorphic flavour of the api
+	 */
 	val tryAndRecover: TopicFlavouredApi<Topic>
 }
 
