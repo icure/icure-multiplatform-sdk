bff8406247e89dca152b9e4ac8bb466acbc0acea
Vincenzo Pierro <vcp@icure.com>
2024-08-22 15:51:53 +0200

    Updated HealthElement filter options

--- icure-sdk/src/commonMain/kotlin/com/icure/sdk/filters/HealthElementFilters.kt
+++ icure-sdk/src/commonMain/kotlin/com/icure/sdk/filters/HealthElementFilters.kt
@@ -7,9 +7,11 @@ import com.icure.sdk.model.HealthElement
 import com.icure.sdk.model.Patient
 import com.icure.sdk.model.base.Identifier
 import com.icure.sdk.model.filter.AbstractFilter
+import com.icure.sdk.model.filter.healthelement.HealthElementByDataOwnerPatientOpeningDate
 import com.icure.sdk.model.filter.healthelement.HealthElementByHcPartyFilter
 import com.icure.sdk.model.filter.healthelement.HealthElementByHcPartyIdentifiersFilter
 import com.icure.sdk.model.filter.healthelement.HealthElementByHcPartySecretForeignKeysFilter
+import com.icure.sdk.model.filter.healthelement.HealthElementByHcPartyStatusFilter
 import com.icure.sdk.model.filter.healthelement.HealthElementByHcPartyTagCodeFilter
 import com.icure.sdk.model.filter.healthelement.HealthElementByIdsFilter
 import com.icure.sdk.utils.DefaultValue
@@ -20,6 +22,7 @@ import kotlinx.serialization.Serializable
 object HealthElementFilters {
     /**
      * Create options for health element filtering that will match all health element shared directly (i.e. ignoring hierarchies) with a specific data owner.
+     *
      * @param dataOwnerId a data owner id
      * @return options for health element filtering
      */
@@ -30,6 +33,7 @@ object HealthElementFilters {
 
     /**
      * Create options for health element filtering that will match all health element shared directly (i.e. ignoring hierarchies) with the current data owner.
+     *
      * @return options for health element filtering
      */
     fun allHealthElementsForSelf(): FilterOptions<HealthElement> =
@@ -43,6 +47,7 @@ object HealthElementFilters {
      * These options are sortable. When sorting using these options the health elements will be in the same order as the input
      * identifiers. In case an entity has multiple identifiers only the first matching identifier is considered for the
      * sorting.
+     *
      * @param dataOwnerId a data owner id or null to use the current data owner id
      * @param identifiers a list of identifiers
      * @return options for health element filtering
@@ -61,6 +66,7 @@ object HealthElementFilters {
      * These options are sortable. When sorting using these options the health elements will be in the same order as the input
      * identifiers. In case an entity has multiple identifiers only the first matching identifier is considered for the
      * sorting.
+     *
      * @param identifiers a list of identifiers
      * @return options for health element filtering
      */
@@ -157,13 +163,14 @@ object HealthElementFilters {
      *
      * When using these options the sdk will automatically extract the secret ids from the provided patients and use
      * those for filtering.
-     * If you already have the secret ids of the patient you may instead use [byPatientsSecretIds].
+     * If you already have the secret ids of the patient you may instead use [byPatientsSecretIdsForDataOwner].
      * If the current data owner does not have access to any secret id of one of the provide patients the patient will
      * simply be ignored.
      * Note that these may not be used in methods of apis from [IcureBaseApis].
      *
      * These options are sortable. When sorting using these options the health elements will be sorted by the patients, using
      * the same order as the input patients.
+     *
      * @param dataOwnerId a data owner id
      * @param patients a list of patients.
      */
@@ -181,27 +188,26 @@ object HealthElementFilters {
      *
      * When using these options the sdk will automatically extract the secret ids from the provided patients and use
      * those for filtering.
-     * If you already have the secret ids of the patient you may instead use [byPatientsSecretIds].
+     * If you already have the secret ids of the patient you may instead use [byPatientsSecretIdsForSelf].
      * If the current data owner does not have access to any secret id of one of the provide patients the patient will
      * simply be ignored.
      * Note that these may not be used in methods of apis from [IcureBaseApis].
      *
      * These options are sortable. When sorting using these options the health elements will be sorted by the patients, using
      * the same order as the input patients.
+     *
      * @param patients a list of patients.
      */
     fun byPatientsForSelf(
         patients: List<Patient>
-    ): SortableFilterOptions<HealthElement> = ByPatientsForSelf(
-            patients = patients
-        )
-
+    ): SortableFilterOptions<HealthElement> = ByPatientsForSelf(patients = patients)
 
     /**
      * Options for health element filtering which match all health elements shared directly (i.e. ignoring hierarchies) with a specific data owner that are linked with a
      * patient through one of the provided secret ids.
      * These options are sortable. When sorting using these options the health elements will be sorted by the linked patients
      * secret id, using the same order as the input.
+     *
      * @param dataOwnerId a data owner id
      * @param secretIds a list of patients secret ids
      */
@@ -222,19 +228,167 @@ object HealthElementFilters {
      */
     fun byPatientsSecretIdsForSelf(
         secretIds: List<String>
-    ): SortableFilterOptions<HealthElement> = ByPatientsSecretIdsForSelf(
-            secretIds = secretIds
-        )
+    ): SortableFilterOptions<HealthElement> = ByPatientsSecretIdsForSelf(secretIds = secretIds)
 
     /**
      * Filter options that match all health elements with one of the provided ids.
      * These options are sortable. When sorting using these options the health elements will have the same order as the input ids.
+     *
      * @param ids a list of unique health element ids.
      * @throws IllegalArgumentException if the provided [ids] list contains duplicate elements
      */
     fun byIds(
         ids: List<String>
     ): BaseSortableFilterOptions<HealthElement> = ByIds(ids)
+
+	/**
+	 * Options for health element filtering which match all health elements shared directly (i.e. ignoring hierarchies) with a specific data owner
+	 * that are linked with one of the provided patients.
+	 * This Options also allows to restrict the health elements based on [HealthElement.openingDate]:
+	 * - if the [from] fuzzy date is not null, only the health elements where [HealthElement.openingDate] is greater than or equal to [from] will be returned.
+	 * - if the [to] fuzzy date is not null, only the health elements where [HealthElement.openingDate] is less than or equal to [to] will be returned.
+	 *
+	 * When using these options the sdk will automatically extract the secret ids from the provided patients and use
+	 * those for filtering.
+	 * If you already have the secret ids of the patient you may instead use [byPatientSecretIdsOpeningDateForDataOwner].
+	 * If the current data owner does not have access to any secret id of one of the provide patients the patient will
+	 * simply be ignored.
+	 * Note that these may not be used in methods of apis from [IcureBaseApis].
+	 *
+	 * These options are sortable. When sorting using these options the health elements will be sorted by [HealthElement.openingDate] in ascending or
+	 * descending order according to the value of the [descending] parameter.
+	 *
+	 * @param dataOwnerId a data owner id
+	 * @param patients a list of patients.
+	 * @param from the minimum fuzzy date for [HealthElement.openingDate], in the YYYYMMDDHHMMSS format (default: no limit).
+	 * @param to the maximum fuzzy date for [HealthElement.openingDate], in the YYYYMMDDHHMMSS format (default: no limit).
+	 * @param descending whether to sort the result in descending or ascending order by [HealthElement.openingDate] (default: ascending).
+	 */
+	fun byPatientsOpeningDateForDataOwner(
+		dataOwnerId: String,
+		patients: List<Patient>,
+		@DefaultValue("null")
+		from: Long? = null,
+		@DefaultValue("null")
+		to: Long? = null,
+		@DefaultValue("false")
+		descending: Boolean = false
+	) : SortableFilterOptions<HealthElement> = ByPatientsOpeningDateForDataOwner(dataOwnerId, patients, from, to, descending)
+
+	/**
+	 * Options for health element filtering which match all health elements shared directly (i.e. ignoring hierarchies) with the current data owner
+	 * that are linked with one of the provided patients.
+	 * This Options also allows to restrict the health elements based on [HealthElement.openingDate]:
+	 * - if the [from] fuzzy date is not null, only the health elements where [HealthElement.openingDate] is greater than or equal to [from] will be returned.
+	 * - if the [to] fuzzy date is not null, only the health elements where [HealthElement.openingDate] is less than or equal to [to] will be returned.
+	 *
+	 * When using these options the sdk will automatically extract the secret ids from the provided patients and use
+	 * those for filtering.
+	 * If you already have the secret ids of the patient you may instead use [byPatientSecretIdsOpeningDateForSelf].
+	 * If the current data owner does not have access to any secret id of one of the provide patients the patient will
+	 * simply be ignored.
+	 * Note that these may not be used in methods of apis from [IcureBaseApis].
+	 *
+	 * These options are sortable. When sorting using these options the health elements will be sorted by [HealthElement.openingDate] in ascending or
+	 * descending order according to the value of the [descending] parameter.
+	 *
+	 * @param patients a list of patients.
+	 * @param from the minimum fuzzy date for [HealthElement.openingDate], in the YYYYMMDDHHMMSS format (default: no limit).
+	 * @param to the maximum fuzzy date for [HealthElement.openingDate], in the YYYYMMDDHHMMSS format (default: no limit).
+	 * @param descending whether to sort the result in descending or ascending order by [HealthElement.openingDate] (default: ascending).
+	 */
+	fun byPatientsOpeningDateForSelf(
+		patients: List<Patient>,
+		@DefaultValue("null")
+		from: Long? = null,
+		@DefaultValue("null")
+		to: Long? = null,
+		@DefaultValue("false")
+		descending: Boolean = false
+	) : SortableFilterOptions<HealthElement> = ByPatientsOpeningDateForSelf(patients, from, to, descending)
+
+	/**
+	 * Options for health element filtering which match all health elements shared directly (i.e. ignoring hierarchies) with a specific data owner
+	 * that are linked with one of the provided patients through one of the provided secret ids.
+	 * This Options also allows to restrict the health elements based on [HealthElement.openingDate]:
+	 * - if the [from] fuzzy date is not null, only the health elements where [HealthElement.openingDate] is greater than or equal to [from] will be returned.
+	 * - if the [to] fuzzy date is not null, only the health elements where [HealthElement.openingDate] is less than or equal to [to] will be returned.
+	 *
+	 * If the current data owner does not have access to any secret id of one of the provide patients the patient will
+	 * simply be ignored.
+	 *
+	 * These options are sortable. When sorting using these options the health elements will be sorted by [HealthElement.openingDate] in ascending or
+	 * descending order according to the value of the [descending] parameter.
+	 *
+	 * @param dataOwnerId the id of a data owner.
+	 * @param secretIds a list of patient secret ids.
+	 * @param from the minimum fuzzy date for [HealthElement.openingDate], in the YYYYMMDDHHMMSS format (default: no limit).
+	 * @param to the maximum fuzzy date for [HealthElement.openingDate], in the YYYYMMDDHHMMSS format (default: no limit).
+	 * @param descending whether to sort the result in descending or ascending order by [HealthElement.openingDate] (default: ascending).
+	 */
+	fun byPatientSecretIdsOpeningDateForDataOwner(
+		dataOwnerId: String,
+		secretIds: List<String>,
+		@DefaultValue("null")
+		from: Long? = null,
+		@DefaultValue("null")
+		to: Long? = null,
+		@DefaultValue("false")
+		descending: Boolean = false
+	) : BaseSortableFilterOptions<HealthElement> = ByPatientSecretIdsOpeningDateForDataOwner(dataOwnerId, secretIds, from, to, descending)
+
+	/**
+	 * Options for health element filtering which match all health elements shared directly (i.e. ignoring hierarchies) with the current data owner
+	 * that are linked with one of the provided patients through one of the provided secret ids.
+	 * This Options also allows to restrict the health elements based on [HealthElement.openingDate]:
+	 * - if the [from] fuzzy date is not null, only the health elements where [HealthElement.openingDate] is greater than or equal to [from] will be returned.
+	 * - if the [to] fuzzy date is not null, only the health elements where [HealthElement.openingDate] is less than or equal to [to] will be returned.
+	 *
+	 * If the current data owner does not have access to any secret id of one of the provide patients the patient will
+	 * simply be ignored.
+	 * Note that these may not be used in methods of apis from [IcureBaseApis].
+	 *
+	 * These options are sortable. When sorting using these options the health elements will be sorted by [HealthElement.openingDate] in ascending or
+	 * descending order according to the value of the [descending] parameter.
+	 *
+	 * @param secretIds a list of patient secret ids.
+	 * @param from the minimum fuzzy date for [HealthElement.openingDate], in the YYYYMMDDHHMMSS format (default: no limit).
+	 * @param to the maximum fuzzy date for [HealthElement.openingDate], in the YYYYMMDDHHMMSS format (default: no limit).
+	 * @param descending whether to sort the result in descending or ascending order by [HealthElement.openingDate] (default: ascending).
+	 */
+	fun byPatientSecretIdsOpeningDateForSelf(
+		secretIds: List<String>,
+		@DefaultValue("null")
+		from: Long? = null,
+		@DefaultValue("null")
+		to: Long? = null,
+		@DefaultValue("false")
+		descending: Boolean = false
+	) : SortableFilterOptions<HealthElement> = ByPatientSecretIdsOpeningDateForSelf(secretIds, from, to, descending)
+
+	/**
+	 * Options for health element filtering which match all health elements shared directly (i.e. ignoring hierarchies) with a specific data owner
+	 * where [HealthElement.status] is equal to status.
+	 *
+	 * @param dataOwnerId the id of a data owner.
+	 * @param status the status to use in the filter.
+	 */
+	fun byStatusForDataOwner(
+		dataOwnerId: String,
+		status: Int
+	) : BaseFilterOptions<HealthElement> = ByStatusForDataOwner(dataOwnerId, status)
+
+	/**
+	 * Options for health element filtering which match all health elements shared directly (i.e. ignoring hierarchies) with the current data owner
+	 * where [HealthElement.status] is equal to status.
+	 *
+	 * Note that these may not be used in methods of apis from [IcureBaseApis].
+	 *
+	 * @param status the status to use in the filter.
+	 */
+	fun byStatusForSelf(
+		status: Int
+	) : FilterOptions<HealthElement> = ByStatusForSelf(status)
 
     @Serializable
     internal class AllForDataOwner(
@@ -311,6 +465,51 @@ object HealthElementFilters {
             ids.requireUniqueElements("`ids`")
         }
     }
+
+	@Serializable
+	internal class ByPatientsOpeningDateForDataOwner(
+		val dataOwnerId: String,
+		val patients: List<Patient>,
+		val from: Long?,
+		val to: Long?,
+		val descending: Boolean
+	) : SortableFilterOptions<HealthElement>
+
+	@Serializable
+	internal class ByPatientsOpeningDateForSelf(
+		val patients: List<Patient>,
+		val from: Long?,
+		val to: Long?,
+		val descending: Boolean
+	) : SortableFilterOptions<HealthElement>
+
+	@Serializable
+	internal class ByPatientSecretIdsOpeningDateForDataOwner(
+		val dataOwnerId: String,
+		val secretIds: List<String>,
+		val from: Long?,
+		val to: Long?,
+		val descending: Boolean
+	) : BaseSortableFilterOptions<HealthElement>
+
+	@Serializable
+	internal class ByPatientSecretIdsOpeningDateForSelf(
+		val secretIds: List<String>,
+		val from: Long?,
+		val to: Long?,
+		val descending: Boolean
+	) : SortableFilterOptions<HealthElement>
+
+	@Serializable
+	internal class ByStatusForDataOwner(
+		val dataOwnerId: String,
+		val status: Int
+	) : BaseFilterOptions<HealthElement>
+
+	@Serializable
+	internal class ByStatusForSelf(
+		val status: Int
+	) : FilterOptions<HealthElement>
 }
 
 @InternalIcureApi
@@ -415,6 +614,58 @@ internal suspend fun mapHealthElementFilterOptions(
             healthcarePartyId = selfDataOwnerId
         )
     }
+	is HealthElementFilters.ByPatientsOpeningDateForDataOwner -> {
+		filterOptions.ensureNonBaseEnvironment(selfDataOwnerId, entityEncryptionService)
+		HealthElementByDataOwnerPatientOpeningDate(
+			healthcarePartyId = filterOptions.dataOwnerId,
+			patientSecretForeignKeys = filterOptions.patients.flatMap {
+				entityEncryptionService.secretIdsOf(it.withTypeInfo(), null)
+			}.toSet(),
+			startDate = filterOptions.from,
+			endDate = filterOptions.to,
+			descending = filterOptions.descending
+		)
+	}
+	is HealthElementFilters.ByPatientsOpeningDateForSelf -> {
+		filterOptions.ensureNonBaseEnvironment(selfDataOwnerId, entityEncryptionService)
+		HealthElementByDataOwnerPatientOpeningDate(
+			healthcarePartyId = selfDataOwnerId,
+			patientSecretForeignKeys = filterOptions.patients.flatMap {
+				entityEncryptionService.secretIdsOf(it.withTypeInfo(), null)
+			}.toSet(),
+			startDate = filterOptions.from,
+			endDate = filterOptions.to,
+			descending = filterOptions.descending
+		)
+	}
+	is HealthElementFilters.ByPatientSecretIdsOpeningDateForDataOwner -> HealthElementByDataOwnerPatientOpeningDate(
+		healthcarePartyId = filterOptions.dataOwnerId,
+		patientSecretForeignKeys = filterOptions.secretIds.toSet(),
+		startDate = filterOptions.from,
+		endDate = filterOptions.to,
+		descending = filterOptions.descending
+	)
+	is HealthElementFilters.ByPatientSecretIdsOpeningDateForSelf -> {
+		filterOptions.ensureNonBaseEnvironment(selfDataOwnerId, entityEncryptionService)
+		HealthElementByDataOwnerPatientOpeningDate(
+			healthcarePartyId = selfDataOwnerId,
+			patientSecretForeignKeys = filterOptions.secretIds.toSet(),
+			startDate = filterOptions.from,
+			endDate = filterOptions.to,
+			descending = filterOptions.descending
+		)
+	}
+	is HealthElementFilters.ByStatusForDataOwner -> HealthElementByHcPartyStatusFilter(
+		hcPartyId = filterOptions.dataOwnerId,
+		status = filterOptions.status
+	)
+	is HealthElementFilters.ByStatusForSelf -> {
+		filterOptions.ensureNonBaseEnvironment(selfDataOwnerId, entityEncryptionService)
+		HealthElementByHcPartyStatusFilter(
+			hcPartyId = selfDataOwnerId,
+			status = filterOptions.status
+		)
+	}
     else -> {
         throw IllegalArgumentException("Filter options ${filterOptions::class.simpleName} are not valid for filtering HealthElements")
     }
