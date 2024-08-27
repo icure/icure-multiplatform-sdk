c60d2fa707c22342f1e578fd8c242a9bbd828c9e
Clément VANDENDAELEN <cv@icure.com>
2024-08-27 13:48:39 +0200

    Exposed RSA Encryption algorithm to XCFramework

--- icure-sdk/src/appleMain/kotlin/com/icure/sdk/utils/DefaultCryptoService.kt
+++ icure-sdk/src/appleMain/kotlin/com/icure/sdk/utils/DefaultCryptoService.kt
@@ -1,7 +1,12 @@
 package com.icure.sdk.utils
 
+import com.icure.kryptom.crypto.RsaAlgorithm
+
 /**
  * This is a default crypto service that is used by the SDK.
  * The sole purpose of this variable is to make the Kotlin Multiplatform to generate the correct code for the XcFramework.
  */
 val defaultCryptoService = com.icure.kryptom.crypto.defaultCryptoService
+
+val rsaEncryptionAlgorithmOaepWithSha1 = RsaAlgorithm.RsaEncryptionAlgorithm.OaepWithSha1
+val rsaEncryptionAlgorithmOaepWithSha256 = RsaAlgorithm.RsaEncryptionAlgorithm.OaepWithSha256
