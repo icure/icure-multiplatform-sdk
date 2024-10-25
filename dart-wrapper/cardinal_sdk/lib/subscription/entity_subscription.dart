954254ab6d0746686a77b7cb222fdeded2e8d0bd
Vincenzo Pierro <vcp@icure.com>
2024-10-25 12:01:04 +0200

    Added user-facing subscription

--- dart-wrapper/cardinal_sdk/lib/subscription/entity_subscription.dart
+++ dart-wrapper/cardinal_sdk/lib/subscription/entity_subscription.dart
@@ -1,6 +1,31 @@
-class EntitySubscription<T> {
+import 'package:cardinal_sdk/model/base/identifiable.dart';
+import 'package:cardinal_sdk/subscription/entity_subscription_event.dart';
+
+class EntitySubscription<T extends Identifiable<String>> {
   final String _instanceId;
   final T Function(Map<String, dynamic>) _decodeT;
 
   EntitySubscription(this._instanceId, this._decodeT);
+
+  Future<void> close(String sdkId) async {
+    CardinalSdkPlatformInterface.instance.subscription.close(_instanceId);
+  }
+
+  Future<EntitySubscriptionCloseReason?> getCloseReason(String sdkId) async {
+    return CardinalSdkPlatformInterface.instance.subscription.getCloseReason(_instanceId);
+  }
+
+  Future<EntitySubscriptionEvent<T>> getEvent(String sdkId) {
+    final eventJson = await CardinalSdkPlatformInterface.instance
+        .subscription
+        .getEvent(_instanceId);
+    return EntitySubscriptionEvent.fromJSON(eventJson, _decodeT);
+  }
+
+  Future<EntitySubscriptionEvent<T>> waitForEvent(String sdkId, timeout: Duration) async {
+    final eventJson = await CardinalSdkPlatformInterface.instance
+        .subscription
+        .waitForEvent(_instanceId, timeout);
+    return EntitySubscriptionEvent.fromJSON(eventJson, _decodeT);
+  }
 }
