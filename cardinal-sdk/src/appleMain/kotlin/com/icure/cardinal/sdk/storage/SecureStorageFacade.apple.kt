537bb397128eeb0f84065fafd2b109817042acd6
Clément VANDENDAELEN <cv@icure.com>
2024-11-18 16:56:15 +0100

    feat: First impl of Apple Secure storage facade

--- cardinal-sdk/src/appleMain/kotlin/com/icure/cardinal/sdk/storage/SecureStorageFacade.apple.kt
+++ cardinal-sdk/src/appleMain/kotlin/com/icure/cardinal/sdk/storage/SecureStorageFacade.apple.kt
@@ -7,12 +7,14 @@ import com.icure.kryptom.crypto.AesKey
 import com.icure.kryptom.crypto.CryptoService
 import io.ktor.utils.io.core.toByteArray
 import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.ULongVar
 import kotlinx.cinterop.alloc
 import kotlinx.cinterop.memScoped
 import kotlinx.cinterop.ptr
 import kotlinx.cinterop.value
 import platform.CoreFoundation.CFDictionaryAddValue
 import platform.CoreFoundation.CFDictionaryCreateMutable
+import platform.CoreFoundation.CFErrorRef
 import platform.CoreFoundation.CFTypeRefVar
 import platform.CoreFoundation.kCFAllocatorDefault
 import platform.CoreFoundation.kCFBooleanTrue
@@ -21,29 +23,34 @@ import platform.CoreFoundation.kCFTypeDictionaryValueCallBacks
 import platform.Foundation.CFBridgingRetain
 import platform.Foundation.NSData
 import platform.Security.SecAccessControlCreateFlags
+import platform.Security.SecAccessControlCreateWithFlags
 import platform.Security.SecItemCopyMatching
+import platform.Security.SecItemAdd
 import platform.Security.errSecSuccess
 import platform.Security.errSecItemNotFound
 import platform.Security.kSecAccessControlBiometryAny
 import platform.Security.kSecAccessControlDevicePasscode
+import platform.Security.kSecAttrAccessibleWhenUnlockedThisDeviceOnly
 import platform.Security.kSecAttrApplicationTag
 import platform.Security.kSecClass
 import platform.Security.kSecClassKey
+import platform.Security.kSecValueData
+import platform.Security.kSecAttrAccessControl
 import platform.Security.kSecMatchLimit
 import platform.Security.kSecMatchLimitOne
 import platform.Security.kSecReturnData
 
 actual suspend fun getOrCreateSecretKey(storageFacade: StorageFacade, key: String, accessLevel: Set<SecureKeyAccessLevel>, cryptoService: CryptoService): AesKey<AesAlgorithm.CbcWithPkcs7Padding> {
-	return getSecretKey(cryptoService) ?: createSecretKey(accessLevel, cryptoService)
+	return getSecretKey(cryptoService, key) ?: createSecretKey(accessLevel, cryptoService, key)
 }
 
 @OptIn(ExperimentalForeignApi::class)
-private suspend fun getSecretKey(cryptoService: CryptoService): AesKey<AesAlgorithm.CbcWithPkcs7Padding>? {
+private suspend fun getSecretKey(cryptoService: CryptoService, key: String): AesKey<AesAlgorithm.CbcWithPkcs7Padding>? {
 	val query = CFDictionaryCreateMutable(
-		kCFAllocatorDefault,
-		4,
-		kCFTypeDictionaryKeyCallBacks.ptr,
-		kCFTypeDictionaryValueCallBacks.ptr
+		allocator = kCFAllocatorDefault,
+		capacity = 4,
+		keyCallBacks = kCFTypeDictionaryKeyCallBacks.ptr,
+		valueCallBacks = kCFTypeDictionaryValueCallBacks.ptr
 	).also {
 		CFDictionaryAddValue(it, kSecClass, kSecClassKey)
 		CFDictionaryAddValue(it, kSecAttrApplicationTag, CFBridgingRetain(key.toByteArray().toNSData()))
@@ -68,11 +75,40 @@ private suspend fun getSecretKey(cryptoService: CryptoService): AesKey<AesAlgori
 	}
 }
 
-private suspend fun createSecretKey(accessLevel: Set<SecureKeyAccessLevel>, cryptoService: CryptoService): AesKey<AesAlgorithm.CbcWithPkcs7Padding> {
+@OptIn(ExperimentalForeignApi::class)
+private suspend fun createSecretKey(accessLevel: Set<SecureKeyAccessLevel>, cryptoService: CryptoService, key: String): AesKey<AesAlgorithm.CbcWithPkcs7Padding> {
+
 	val aesKey = cryptoService.aes.generateKey(AesAlgorithm.CbcWithPkcs7Padding)
 	val bytes = cryptoService.aes.exportKey(aesKey)
 
-	val accessControl: SecAccessControlCreateFlags = accessLevel.first().toSecAccessControlCreateFlags()
+	val accessControlFlags: SecAccessControlCreateFlags = accessLevel.fold(0.toULong()) { acc, level -> acc or level.toSecAccessControlCreateFlags() }
+
+	val accessControl = SecAccessControlCreateWithFlags(
+		allocator = kCFAllocatorDefault,
+		protection = kSecAttrAccessibleWhenUnlockedThisDeviceOnly,
+		flags = accessControlFlags,
+		error = null
+	)
+
+	val query = CFDictionaryCreateMutable(
+		allocator = kCFAllocatorDefault,
+		capacity = 4,
+		keyCallBacks = kCFTypeDictionaryKeyCallBacks.ptr,
+		valueCallBacks = kCFTypeDictionaryValueCallBacks.ptr
+	).also {
+		CFDictionaryAddValue(it, kSecClass, kSecClassKey)
+		CFDictionaryAddValue(it, kSecAttrApplicationTag, CFBridgingRetain(key.toByteArray().toNSData()))
+		CFDictionaryAddValue(it, kSecValueData, CFBridgingRetain(bytes.toNSData()))
+		CFDictionaryAddValue(it, kSecAttrAccessControl, accessControl)
+	}
+
+	val status = SecItemAdd(query, null)
+
+	if (status == errSecSuccess) {
+		return aesKey
+	} else {
+		throw IllegalStateException("Failed to generate key data")
+	}
 }
 
 private fun SecureKeyAccessLevel.toSecAccessControlCreateFlags(): SecAccessControlCreateFlags {
