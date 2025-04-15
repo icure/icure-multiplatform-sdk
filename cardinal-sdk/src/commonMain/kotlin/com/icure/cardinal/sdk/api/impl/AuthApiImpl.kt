2f980d0e6c19efe862bda3c685c164ad744cdc06
Vincenzo Pierro <vcp@icure.com>
2025-04-15 17:44:01 +0200

    Fixed instance type check on AuthApi

--- cardinal-sdk/src/commonMain/kotlin/com/icure/cardinal/sdk/api/impl/AuthApiImpl.kt
+++ cardinal-sdk/src/commonMain/kotlin/com/icure/cardinal/sdk/api/impl/AuthApiImpl.kt
@@ -2,13 +2,13 @@ package com.icure.cardinal.sdk.api.impl
 
 import com.icure.cardinal.sdk.api.AuthApi
 import com.icure.cardinal.sdk.auth.services.AuthProvider
-import com.icure.cardinal.sdk.auth.services.JwtAuthProvider
+import com.icure.cardinal.sdk.auth.services.JwtBasedAuthProvider
 import com.icure.utils.InternalIcureApi
 
 @OptIn(InternalIcureApi::class)
 class AuthApiImpl(val authProvider: AuthProvider) : AuthApi {
 	override suspend fun getBearerToken(): String {
-		if (authProvider is JwtAuthProvider) {
+		if (authProvider is JwtBasedAuthProvider) {
 			return authProvider.getBearerAndRefreshToken().bearer.token
 		} else {
 			throw UnsupportedOperationException("The AuthProvider ${authProvider::class} does not support bearer tokens")
