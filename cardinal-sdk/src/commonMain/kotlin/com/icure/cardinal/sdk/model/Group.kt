63bb8f8f94174df2a73499ef928c3f17bbea8123
Luca Tremamunno <luca@icure.com>
2025-04-17 10:36:06 +0200

    Remove unused import

--- cardinal-sdk/src/commonMain/kotlin/com/icure/cardinal/sdk/model/Group.kt
+++ cardinal-sdk/src/commonMain/kotlin/com/icure/cardinal/sdk/model/Group.kt
@@ -15,7 +15,6 @@ import kotlin.String
 import kotlin.collections.List
 import kotlin.collections.Map
 import kotlin.collections.Set
-import com.icure.cardinal.sdk.model.security.ExternalJwtSelector
 
 // WARNING: This file is auto-generated. If you change it manually, your changes will be lost.
 // If you want to change the way this class is generated, see [this repo](https://github.com/icure/sdk-codegen).
