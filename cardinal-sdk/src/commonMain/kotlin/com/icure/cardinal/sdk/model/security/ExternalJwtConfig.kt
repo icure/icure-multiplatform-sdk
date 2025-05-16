1d463ebe3c05917e6ca0c87b33456bdfe6024738
Vincenzo Pierro <vcp@icure.com>
2025-05-16 14:15:05 +0200

    Added serial name on ExternalJwtConfig

--- cardinal-sdk/src/commonMain/kotlin/com/icure/cardinal/sdk/model/security/ExternalJwtConfig.kt
+++ cardinal-sdk/src/commonMain/kotlin/com/icure/cardinal/sdk/model/security/ExternalJwtConfig.kt
@@ -5,6 +5,7 @@ import com.icure.cardinal.sdk.utils.DefaultValue
 import kotlinx.serialization.Serializable
 import kotlin.String
 import com.icure.cardinal.sdk.model.specializations.Base64String
+import kotlinx.serialization.SerialName
 
 // WARNING: This file is auto-generated. If you change it manually, your changes will be lost.
 // If you want to change the way this class is generated, see [this repo](https://github.com/icure/sdk-codegen).
@@ -20,11 +21,13 @@ data class ExternalJwtConfig(
 	@Serializable
 	public sealed interface ValidationMethod {
 		@Serializable
+		@SerialName("PublicKey")
 		public data class PublicKey(
 			public val key: String,
 		) : ValidationMethod
 
 		@Serializable
+		@SerialName("Oidc")
 		public data class Oidc(
 			public val issureLocation: String,
 		) : ValidationMethod
@@ -33,26 +36,31 @@ data class ExternalJwtConfig(
 	@Serializable
 	public sealed interface FieldSelector {
 		@Serializable
+		@SerialName("LocalId")
 		public data class LocalId(
 			public val fieldName: String,
 		) : FieldSelector
 
 		@Serializable
+		@SerialName("Email")
 		public data class Email(
 			public val fieldName: String,
 		) : FieldSelector
 
 		@Serializable
+		@SerialName("MobilePhone")
 		public data class MobilePhone(
 			public val fieldName: String,
 		) : FieldSelector
 
 		@Serializable
+		@SerialName("Username")
 		public data class Username(
 			public val fieldName: String,
 		) : FieldSelector
 
 		@Serializable
+		@SerialName("Identifier")
 		public data class Identifier(
 			public val identifierAssigner: String,
 			public val fieldName: String,
